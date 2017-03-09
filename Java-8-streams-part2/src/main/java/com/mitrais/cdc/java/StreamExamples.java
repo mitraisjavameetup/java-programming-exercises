eabd97521de2097a22fba95f4e88b1d2beddde69
Marissa Hamnon <Marissa.Hamnon@mitrais.com>
2017-03-09 16:51:50 +0700

    add Java 8 streams part 2 exercise and Java 8 streams part 3 exercise

---
+++ Java-8-streams-part2/src/main/java/com/mitrais/cdc/java/StreamExamples.java
@@ -0,0 +1,81 @@
+package com.mitrais.cdc.java;
+
+import java.util.*;
+
+/** TO DO
+ *  please complete the methods for below capabilities (you should using lambda expression).
+ *  1. Produce a single String that is the result of concatenating the uppercase versions of all of the 
+ *  Strings. E.g., the result should be "HIHELLOHOLA...". Use a single reduce operation, without using map.
+ *  
+ *  2. Produce the same String as above, but this time via a map operation that turns the words 
+ *  into uppercase, followed by a reduce operation that concatenates them.
+ *  
+ *  3. Produce a String that is all the words concatenated together, but with commas in between. 
+ *  E.g., the result should be "hi,hello,...". 
+ *  Note that there is no comma at the beginning, before “hi” 
+ *  and also no comma at the end (after the last word)
+ *  
+ *  4. Find the total number of characters (i.e., sum of the lengths) of the strings in the List
+ *  
+ *  5. Find the number of words that contain an “h”
+ */
+
+public class StreamExamples {
+	
+	List<String> words;
+	StreamExamples(){
+		words = Arrays.asList("hi", "hello", "hola", "bye", "goodbye", "adios");
+	}
+	
+	public List<String> getStringList(){
+		return words;
+	}
+	
+	public String uppercaseWithoutMap(){
+		String uppercaseString = null;
+		/* TO DO
+		 * complete this method to answer number 1
+		 * using lambda expression
+		 */
+		
+		return uppercaseString;
+	}
+	
+	public String uppercaseWithMap(){
+		String uppercaseString = null;
+		/* TO DO
+		 * complete this method to answer number 2
+		 * using lambda expression
+		 */
+		
+		return uppercaseString;
+	}
+	
+	public String wordsWithComma(){
+		String convertWords = null;
+		/* TO DO
+		 * complete this method to answer number 3
+		 * using lambda expression
+		 */
+		
+		return convertWords;
+	}
+	
+	public int numberOfCharacters(){
+		int numOfChar = 0;
+		/*TO DO
+		 *complete this method to answer number 4
+		 *using lambda expression
+		 */
+		return numOfChar;
+	}
+	
+	public int numberOfWordsWithH(){
+		int numOfH = 0;
+		/*TO DO
+		 *complete this method to answer number 5
+		 *using lambda expression
+		 */
+		return numOfH;
+	}
+}
---
+++ Java-8-streams-part3/src/main/java/com/mitrais/cdc/java/StreamExamples.java
@@ -0,0 +1,59 @@
+package com.mitrais.cdc.java;
+
+import java.util.*;
+import java.util.stream.*;
+
+/** TO DO 
+ *  - You will generate very large array of random doubles 
+ *  arraySize: 100_000_000
+ *  (you can use randomArray method on MathUtils.java)
+ *  - Compute the sum of the square roots of the numbers in the array 
+ *  Find a shorter and simpler way than making a loop to tally the sum
+ *  (You could use available method sqrtSum and sqrtSumParallel on MathUtils.java) 
+ *  - Test whether the parallel approach is faster than the sequential approach
+ *  using timeOp method on Op.java
+ */
+
+public class StreamExamples {
+	
+	int arraySize = 1_000_000;
+  
+	public void runStreamExamples(){
+	//get the array and store to variable
+	//print the randomArray
+	//Call method to sum sqrt with sequential approach
+	//print the sum
+	//Call method to sum sqrt with parallel approach
+	//print the sum
+	//Call to get time to sum sequential approach and print the result
+	//Call to get time to sum parallel approach and print the result
+	} 
+  
+	public double[] getArray(){
+		double[] numArray = MathUtils.randomArray(arraySize);
+		return numArray;
+	}
+	
+	public double timeSumSequential(DoubleStream numStream) {
+		double time = 0;
+	  /* TO DO
+	   * using lambda expression
+	   * call method/function to count the elapsed time on Op.java
+	   * with operation is sqrtSum
+	   * then stored to variable time
+	   */
+		return time;
+	}
+  
+	public double timeSumParallel(DoubleStream numStream) {
+		double time = 0;
+	  /* TO DO
+	   * using lambda expression
+	   * call method/function to count the elapsed time on Op.java
+	   * with operation is sqrtSumParallel
+	   * then stored to variable time
+	   */
+		return time;
+	}
+  
+}
