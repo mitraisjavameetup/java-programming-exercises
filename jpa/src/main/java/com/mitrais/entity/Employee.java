d1bc4e1751c4c879c091e7f77590fd666f6f600d
rubenjoy <rubenjoy@gmail.com>
2017-03-10 16:12:23 +0800

    fixing implicit import

--- jpa/src/main/java/com/mitrais/entity/Employee.java
+++ jpa/src/main/java/com/mitrais/entity/Employee.java
@@ -4,7 +4,24 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
-import javax.persistence.*;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
 
 /**
  *   Employee is entity mapped to table
