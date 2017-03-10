7c73ba4c494a4cf7a2d3e57d6090fbabfb672fb0
ilmiawan <muslim.ilmiawan@mitrais.com>
2017-03-10 11:04:18 +0700

    Add static default method in java 8 Exercise files

---
+++ static-default-methods/src/main/java/com/mitrais/cdc/java/EquilateralTriangle.java
@@ -0,0 +1,9 @@
+package com.mitrais.cdc.java;
+
+public class EquilateralTriangle implements RegularPolygon {
+	/* To Do :
+	 * Create a class EquilateralTriangle that implements the interface, 
+	 *     has getNumSides return 3 and getSideLength return an instance variable that is set by the constructor.
+	 */
+	
+}
---
+++ static-default-methods/src/main/java/com/mitrais/cdc/java/Square.java
@@ -0,0 +1,10 @@
+package com.mitrais.cdc.java;
+
+public class Square implements RegularPolygon {
+	
+	/* To Do :
+	 * Create a class EquilateralTriangle that implements the interface, 
+	 *     has getNumSides return 4 and getSideLength return an instance variable that is set by the constructor.
+	 */
+	
+}
