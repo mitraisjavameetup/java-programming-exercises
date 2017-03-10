089ae560f6ffa3efdc8ac6daf16913a9e32a98f2
Marissa Hamnon <Marissa.Hamnon@mitrais.com>
2017-03-10 13:47:10 +0700

    add oop-more exercise

---
+++ oop-more/src/main/java/com/mitrais/cdc/java/Circle.java
@@ -0,0 +1,15 @@
+package com.mitrais.cdc.java;
+
+/** Pre-condition:
+ *  It should continue from your Circle.java on oop-basic exercise
+ *  TO DO:
+ *  1. Change radius field to private
+ *  2. Declare set and get method for radius field so it can be accessed as a public
+ *  Note: 1 and 2 are used to illustrate encapsulation
+ *  3. Add new method toString that return String of information of Circle itself
+ *  e.g : the return will be String as "Circle (radius=0.26359375131266183, area=0.21828309061994705)"
+ */
+
+public class Circle {
+
+}
---
+++ oop-more/src/main/java/com/mitrais/cdc/java/CircleTest.java
@@ -0,0 +1,16 @@
+package com.mitrais.cdc.java;
+
+/** 
+ * TO DO:
+ * run the Circle class
+ * try to generate 1 Circle
+ * and print out the return of toString method
+ * but you don't explicitly call toString method in your code
+ * HINT: use override when you declare toString method on Circle.java
+ */
+
+public class CircleTest {
+  public static void main(String[] args) {
+	  
+  }
+}
