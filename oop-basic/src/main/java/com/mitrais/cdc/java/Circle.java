64349089e09ba6a7206cd1d1d6c7b95b59eda009
trainee <trainee>
2017-03-17 11:24:01 +0700

    Bery Orindi

--- oop-basic/src/main/java/com/mitrais/cdc/java/Circle.java
+++ oop-basic/src/main/java/com/mitrais/cdc/java/Circle.java
@@ -2,14 +2,30 @@ package com.mitrais.cdc.java;
 
 public class Circle  {
 
+	// 1
 	public double radius;
 
+	// 2
+	public double getArea(){
+		return Math.PI * this.radius * this.radius;
+	}
+	
+	// 3 and 4
+	public Circle(double radius){
+		this.radius = radius; 
+	}
+	
+	//5
+	public String toString(){
+		return "radius = " + this.radius;
+	}
+	
 	/*
 	 * todo: make a Circle class with various capabilities below:
 	 * 1. Give the Circle class a radius field of type double
 	 * 2. Give Circle a method that computes the area ((Math.PI)*radius*radius) 
 	 * 3. Give Circle a constructor to which you can pass the radius
-	 * 4. Have the constructor use the “this” variable
+	 * 4. Have the constructor use the â€œthisâ€� variable
 	 * 5. Add a toString method
 	 */
 	
