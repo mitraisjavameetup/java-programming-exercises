2ded686436658fee5a5e0c3d845fe19d4bce14d4
trainee <trainee>
2017-03-17 15:06:28 +0700

    Add java doc

--- oop-more/src/main/java/com/mitrais/cdc/java/Circle.java
+++ oop-more/src/main/java/com/mitrais/cdc/java/Circle.java
@@ -1,21 +1,20 @@
 package com.mitrais.cdc.java;
 
-/** Pre-condition:
- *  It should continue from your Circle.java on oop-basic exercise
- *  TO DO:
- *  1. Change radius field to private
- *  2. Declare set and get method for radius field so it can be accessed as a public
- *  Note: 1 and 2 are used to illustrate encapsulation
- *  3. Add new method toString that return String of information of Circle itself
- *  e.g : the return will be String as "Circle (radius=0.26359375131266183, area=0.21828309061994705)"
+/**
+ * Pre-condition: It should continue from your Circle.java on oop-basic exercise
+ * TO DO: 1. Change radius field to private 2. Declare set and get method for
+ * radius field so it can be accessed as a public Note: 1 and 2 are used to
+ * illustrate encapsulation 3. Add new method toString that return String of
+ * information of Circle itself e.g : the return will be String as "Circle
+ * (radius=0.26359375131266183, area=0.21828309061994705)"
  */
 
 public class Circle {
-	
-	public Circle(double radius){
+
+	public Circle(double radius) {
 		this.radius = radius;
 	}
-	
+
 	private double radius;
 
 	public double getRadius() {
@@ -25,14 +24,14 @@ public class Circle {
 	public void setRadius(double radius) {
 		this.radius = radius;
 	}
-	
-	public double getArea(){
-		return Math.PI*radius*radius;
+
+	public double getArea() {
+		return Math.PI * radius * radius;
 	}
-	
+
 	@Override
 	public String toString() {
-		return "Circle (radius=" + radius + " , area="+ getArea() +")";
+		return "Circle (radius=" + radius + " , area=" + getArea() + ")";
 	}
 
 }
--- oop-more/src/main/java/com/mitrais/cdc/java/Rectangle.java
+++ oop-more/src/main/java/com/mitrais/cdc/java/Rectangle.java
@@ -1,47 +1,48 @@
 package com.mitrais.cdc.java;
 
-/** Pre-condition:
- *  It should continue from your Rectangle.java on oop-basic exercise
- *  TO DO:
- *  1. Change width and height field to private
- *  2. Declare set and get method for width and height fields so it can be accessed as a public
- *  Note: 1 and 2 are used to illustrate encapsulation
- *  3. Add new method toString that return String of information of Rectangle itself
- *  e.g : the return will be String as "Rectangle (width=1.6965338407715858, height=4.389922209793273, area=7.4476515872690685)"
+/**
+ * Pre-condition: It should continue from your Rectangle.java on oop-basic
+ * exercise TO DO: 1. Change width and height field to private 2. Declare set
+ * and get method for width and height fields so it can be accessed as a public
+ * Note: 1 and 2 are used to illustrate encapsulation 3. Add new method toString
+ * that return String of information of Rectangle itself e.g : the return will
+ * be String as "Rectangle (width=1.6965338407715858, height=4.389922209793273,
+ * area=7.4476515872690685)"
  */
 
 public class Rectangle {
-	
+
 	private double width;
 	private double height;
-	
-	public Rectangle(double width, double height){
+
+	public Rectangle(double width, double height) {
 		this.width = width;
 		this.height = height;
 	}
-	
+
 	public double getWidth() {
 		return width;
 	}
+
 	public void setWidth(double width) {
 		this.width = width;
 	}
+
 	public double getHeight() {
 		return height;
 	}
+
 	public void setHeight(double height) {
 		this.height = height;
 	}
-	
-	public double getArea(){
+
+	public double getArea() {
 		return width * height;
 	}
-	
+
 	@Override
 	public String toString() {
-		return "Rectangle (width=" + width + " , height=" + height + " , area="+getArea()+")";
+		return "Rectangle (width=" + width + " , height=" + height + " , area=" + getArea() + ")";
 	}
-	
-	
 
 }
