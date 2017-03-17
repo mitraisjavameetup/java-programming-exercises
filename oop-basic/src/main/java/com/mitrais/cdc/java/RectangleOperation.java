beff7b748aaa5d737c23b05fb88c853e497d70d8
trainee <trainee>
2017-03-17 11:13:16 +0700

    VERNANDO

--- oop-basic/src/main/java/com/mitrais/cdc/java/RectangleOperation.java
+++ oop-basic/src/main/java/com/mitrais/cdc/java/RectangleOperation.java
@@ -10,9 +10,14 @@ public class RectangleOperation {
 	    /*TO DO
 	     * create looping to stored random using Math.random
 	     */
+		for(int i=0;i<rectangles.length;i++){
+			rectangles[i] = new Rectangle();
+			rectangles[i].setRect(Math.random(), Math.random());
+		}
 	}
 	
 	public Rectangle[] getRandomRectangle(){
+		setRandomRectangle();
 		return rectangles;
 	}
 	
@@ -20,10 +25,17 @@ public class RectangleOperation {
 		/*TO DO
 		 * Create codes to get the smallest area from squares array
 		 */
+		this.smallest = Double.MAX_VALUE;
+		for(Rectangle r: rectangles) {
+	      double area = r.getArea();
+	      if (area < this.smallest) {
+	        this.smallest = area;
+	      }
+		}
 	}
 	
 	public double getSmallest(){
-		
+		setSmallest(rectangles);
 		return smallest;
 	}
 	
@@ -31,10 +43,17 @@ public class RectangleOperation {
 		/*TO DO
 		 * Create codes to get the largest area from squares array
 		 */
+		this.largest = 0;
+		for(Rectangle r: rectangles) {
+	      double area = r.getArea();
+	      if (area > this.largest) {
+	        this.largest = area;
+	      }
+		}
 	}
 	
 	public double getLargest(){
-		
+		setLargest(rectangles);
 		return largest;
 	}
 	
@@ -42,10 +61,14 @@ public class RectangleOperation {
 		/*TO DO
 		 * Create codes to get the sum area from squares array
 		 */
+	    for(Rectangle r: rectangles) {
+		      double area = r.getArea();
+		      this.sum = this.sum + area;
+		    }
 	}
 	
 	public double getSum(){
-		
+		setSum(rectangles);
 		return sum;
 	}
 
--- oop-basic/src/main/java/com/mitrais/cdc/java/SquareOperation.java
+++ oop-basic/src/main/java/com/mitrais/cdc/java/SquareOperation.java
@@ -10,9 +10,14 @@ public class SquareOperation {
 	    /*TO DO
 	     * create looping to stored random using Math.random
 	     */
+		for(int i=0;i<squares.length;i++){
+			squares[i] = new Square();
+			squares[i].setWidth(Math.random());
+		}
 	}
 	
 	public Square[] getRandomSquare(){
+		setRandomSquare();
 		return squares;
 	}
 	
@@ -20,10 +25,17 @@ public class SquareOperation {
 		/*TO DO
 		 * Create codes to get the smallest area from squares array
 		 */
+		this.smallest = Double.MAX_VALUE;
+		for(Square s: squares) {
+	      double area = s.getArea();
+	      if (area < this.smallest) {
+	        this.smallest = area;
+	      }
+		}
 	}
 	
 	public double getSmallest(){
-		
+		setSmallest(squares);
 		return smallest;
 	}
 	
@@ -31,10 +43,17 @@ public class SquareOperation {
 		/*TO DO
 		 * Create codes to get the largest area from squares array
 		 */
+		this.largest = 0;
+		for(Square s: squares) {
+	      double area = s.getArea();
+	      if (area > this.largest) {
+	        this.largest = area;
+	      }
+		}
 	}
 	
 	public double getLargest(){
-		
+		setLargest(squares);
 		return largest;
 	}
 	
@@ -42,10 +61,14 @@ public class SquareOperation {
 		/*TO DO
 		 * Create codes to get the sum area from squares array
 		 */
+	    for(Square s: squares) {
+		      double area = s.getArea();
+		      this.sum = this.sum + area;
+		    }
 	}
 	
 	public double getSum(){
-		
+		setSum(squares);
 		return sum;
 	}
 }
