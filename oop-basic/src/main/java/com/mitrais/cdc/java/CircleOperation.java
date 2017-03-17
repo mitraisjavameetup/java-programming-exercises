d3c367fff6f9d1e14a1269eed9bd023464aad4d3
trainee <trainee>
2017-03-17 11:07:01 +0700

    added method definitions

--- oop-basic/src/main/java/com/mitrais/cdc/java/CircleOperation.java
+++ oop-basic/src/main/java/com/mitrais/cdc/java/CircleOperation.java
@@ -1,40 +1,58 @@
 package com.mitrais.cdc.java;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 public class CircleOperation {
 	public int intRandom = 100;
 	public double smallest, largest, sum = 0;
 	Circle[] circles;
 	
 	public void setRandomCircle(){
-		circles = new Circle[intRandom];
-	    /*TO DO
+		/*TO DO
 	     * create looping to stored random using Math.random
 	     */
+		circles = new Circle[intRandom];
+		for (int i = 0; i < intRandom; i++) {
+			circles[i] = new Circle(Math.random() * 10);
+		}
 	}
 	
 	public Circle[] getRandomCircle(){
+		setRandomCircle();
 		return circles;
 	}
 	
-	public void setSmallest(Circle[] circles){
+	public void setSmallest(Circle[] circles) {
 		/*TO DO
 		 * Create codes to get the smallest area from circles array
 		 */
+		smallest = circles[0].getArea();
+		for (int i = 1; i < intRandom; i++) {
+			if (circles[i].getArea() < smallest) {
+				smallest = circles[i].getArea();
+			}
+		}
 	}
 	
-	public double getSmallest(){
-		
+	public double getSmallest() {
+		setSmallest(circles);
 		return smallest;
 	}
 	
-	public void setLargest(Circle[] circles){
+	public void setLargest(Circle[] circles) {
 		/*TO DO
 		 * Create codes to get the largest area from circles array
 		 */
+		largest = circles[0].getArea();
+		for (int i = 1; i < intRandom; i++) {
+			if (circles[i].getArea() > largest) {
+				largest = circles[i].getArea();
+			}
+		}
 	}
 	
-	public double getLargest(){
-		
+	public double getLargest() {
+		setLargest(circles);
 		return largest;
 	}
 	
@@ -42,10 +60,13 @@ public class CircleOperation {
 		/*TO DO
 		 * Create codes to get the sum area from circles array
 		 */
+		for (int i = 0; i < intRandom; i++) {
+			sum += circles[i].getArea();
+		}
 	}
 	
 	public double getSum(){
-		
+		setSum(circles);
 		return sum;
 	}
 }
