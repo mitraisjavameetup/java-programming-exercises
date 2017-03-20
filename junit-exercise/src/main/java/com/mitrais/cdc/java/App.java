0eb0408bf85b7061c4f8999e6541839cbfc470d3
trainee <trainee>
2017-03-20 16:52:09 +0800

    Satriya Wicaksana -Junit exercise +added Java doc

--- junit-exercise/src/main/java/com/mitrais/cdc/java/App.java
+++ junit-exercise/src/main/java/com/mitrais/cdc/java/App.java
@@ -9,26 +9,38 @@ import java.util.List;
  */
 public class App {
 
-	  /** Method calmString:
-	   *  Takes a String and returns a new String with all the exclamation points
-	   *  removed. For example, Utils.calmString("!foo!!") should return "foo".
-	   */
-	  public static String calmString(String speech) {
+	/** Method calmString:
+	 *  Takes a String and returns a new String with all the exclamation points
+	 *  removed. For example, Utils.calmString("!foo!!") should return "foo".
+	 */
+	/** function to remove all the exclamation points **/
+	public static String calmString(String speech) {
 		//todo: apply logic as described above
-
-	    return(speech);
-	  }
-	  
-	  /** Method calmAttendees: 
-	   *  Takes a List of political parties (List of String) and potentially modifies it so
-	   *  that it never contains both "Republican" and "Democrat". If it contains
-	   *  both, it flips a coin to decide which to remove (half time it remove "Republican"
-	   *  and half time it remove "Democrat"
-	   *  If it contains neither,or is null, it does nothing.
-	   */
-	  public static void calmAttendees(List<String> attendingParties) {
-			//todo: apply logic as described above
-	  }
-	  
-	  private App() {} // Class cannot be instantiated: only static methods
+		speech = speech.replaceAll("!", "");
+		return speech;
 	}
+
+	/** Method calmAttendees: 
+	 *  Takes a List of political parties (List of String) and potentially modifies it so
+	 *  that it never contains both "Republican" and "Democrat". If it contains
+	 *  both, it flips a coin to decide which to remove (half time it remove "Republican"
+	 *  and half time it remove "Democrat"
+	 *  If it contains neither,or is null, it does nothing.
+	 */
+	/** function to remove one of the republican or democrat **/
+	public static void calmAttendees(List<String> attendingParties) {
+		//todo: apply logic as described above
+		boolean flagRep = false, flagDem = false;
+		if(attendingParties.contains("Republican") && attendingParties.contains("Democrat")){
+			if(Math.random()>=0.5){
+				attendingParties.remove("Republican");
+			}
+			else{
+				attendingParties.remove("Democrat");
+			}
+		}
+
+	}
+
+	private App() {} // Class cannot be instantiated: only static methods
+}
