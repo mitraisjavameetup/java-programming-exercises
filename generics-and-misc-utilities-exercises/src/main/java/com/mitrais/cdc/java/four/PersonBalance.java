529220671034a160312d23c11be44ec5585b6193
agriardyan <aagriard@gmail.com>
2017-03-20 14:54:44 +0700

    done generic-and-misc-utilities-exercises

--- generics-and-misc-utilities-exercises/src/main/java/com/mitrais/cdc/java/four/PersonBalance.java
+++ generics-and-misc-utilities-exercises/src/main/java/com/mitrais/cdc/java/four/PersonBalance.java
@@ -6,7 +6,44 @@ public class PersonBalance {
 	
 	//todo:
 	//- create constructor
+	/**
+	 * @param name
+	 * @param balance
+	 */
+	public PersonBalance(String name, double balance) {
+		super();
+		this.name = name;
+		this.balance = balance;
+	}
+	
 	//- create setter/getter for the attributes/properties
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * @return the balance
+	 */
+	public double getBalance() {
+		return balance;
+	}
+
+	/**
+	 * @param balance the balance to set
+	 */
+	public void setBalance(double balance) {
+		this.balance = balance;
+	}
 
 	public void printThis() {
 		//Assume that the names are 7 characters or fewer
@@ -14,6 +51,9 @@ public class PersonBalance {
 		//TODO: create value that lined up like this
 		//Juanita's bank account balance is $2,345,678.99.
 		//   Juan's bank account balance is $   15,455.26.
+		System.out.printf("%7s's bank account balance is $%,12.2f %n", this.name, this.balance);
 		
 	}
+
+	
 }
