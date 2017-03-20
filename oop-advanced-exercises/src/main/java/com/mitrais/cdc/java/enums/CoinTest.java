93fc854d03734f3133e8580ce48f83ba9bef9bd1
trainee <trainee>
2017-03-20 09:45:31 +0700

    hafieludin yusuf rizana oop-advance

---
+++ oop-advanced-exercises/src/main/java/com/mitrais/cdc/java/enums/CoinTest.java
@@ -0,0 +1,17 @@
+package com.mitrais.cdc.java.enums;
+
+/**
+ * 
+ * @author trainee
+ * class to test coin flip
+ */
+public class CoinTest{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=1;i<=10;i++){
+			System.out.println(Coin.flip());
+		}
+	}
+
+}
--- oop-advanced-exercises/src/main/java/com/mitrais/cdc/java/resources/PaperclipSale.java
+++ oop-advanced-exercises/src/main/java/com/mitrais/cdc/java/resources/PaperclipSale.java
@@ -3,54 +3,56 @@ package com.mitrais.cdc.java.resources;
 public class PaperclipSale implements Sellable {
 
     private String color;
-    private double boxPrice;
+	private double boxPrice;
     private int numBoxes;
 	
   public PaperclipSale(String color, double boxPrice, int numBoxes) {
 	//TODO: complete this so it will set the properties when called   
-
-  
-  
-  
+	  this.color=color;
+	  this.boxPrice=boxPrice;
+	  this.numBoxes=numBoxes;
   }
-
   //TODO: define getter each attributes/properties
-
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
+  /**
+   * get Color box value
+   * @return String
+   */
+  public String getColor() {
+		return color;
+	}
+  /**
+   * get BoxPrice value
+   * @return double
+   */
+  public double getBoxPrice() {
+		return boxPrice;
+	}
+  /**
+   * get NumBoxes value
+   * @return int 
+   */
+  public int getNumBoxes() {
+		return numBoxes;
+	}
 
   @Override
   /**
    * Will return cost of the price per box (times) number of boxes
    */
   public double getCost() {
-	double finalCost = 0;
-		
+	double finalCost = 0;	
 	//TODO: create calculation to calculate finalCost based on price and number of boxes here
-
-	
-	
-	
-	
+	finalCost=boxPrice*numBoxes;	
 	return(finalCost);  	  
   }
-
+/**
+ * printout all instance of class
+ */
   @Override
   public String toString() {
 	String toString = new String();
-	
 	//TODO: modify this method to return sentence e.g. "This paperclip name is blablabla, its price is blabla, with number of box blabla"
-	
-	  
-	  
+	toString="This paperclip color is "+color+", its price is "+boxPrice+", with number of box "+numBoxes;
 	return toString;  
   }
 }
