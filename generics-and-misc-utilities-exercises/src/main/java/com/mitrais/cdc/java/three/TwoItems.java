f535de06bfe735c767e0df1d2a9a93b2613cbc56
Bintoro Adi Guna <trainee@MTPC216.mitrais.com>
2017-03-20 14:34:34 +0700

    Finished Generic and Misc. Utilities Exercise

--- generics-and-misc-utilities-exercises/src/main/java/com/mitrais/cdc/java/three/TwoItems.java
+++ generics-and-misc-utilities-exercises/src/main/java/com/mitrais/cdc/java/three/TwoItems.java
@@ -2,18 +2,29 @@ package com.mitrais.cdc.java.three;
 
 public class TwoItems<T> {
 	//TODO: define two generic type here
+	private T item1;
+	private T item2;
 
 	//TODO: define parameterized constructor here
+	public TwoItems(T item1, T item2)
+	{
+		this.item1 = item1;
+		this.item2 = item2;
+	}
 
 	//TODO: define getter for those two items defined above
+	public T getItem1() {
+		return item1;
+	}
+
+	public T getItem2() {
+		return item2;
+	}
 	
 	//TODO: override toString method here
 	@Override
 	public String toString(){
-		String returnString = new String();
-		
-		
-		
+		String returnString = "Item 1: " + item1 + ". Item 2: " + item2;
 		return returnString;
 	}
 }
