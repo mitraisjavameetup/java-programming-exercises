c474a9a04ba5bd5274f47381d325a81a59310360
trainee <trainee>
2017-03-20 11:56:52 +0700

    changed on parameter Employee

--- list-map/src/main/java/com/mitrais/cdc/java/EmployeeMap.java
+++ list-map/src/main/java/com/mitrais/cdc/java/EmployeeMap.java
@@ -19,14 +19,14 @@ public class EmployeeMap {
 	 * The point here is to associate keys with values, then retrieve values later based on keys.
 	 */
 	
-	public String runEmployeeMap() {
+	public String runEmployeeMap(String k) {
 		Map<String, String> eMap = new HashMap<>();
 		eMap.put("a1234", "Steve Jobs");
 		eMap.put("a1235", "Scott McNealy");
 		eMap.put("a1236", "Jeff Bezos");
 		eMap.put("a1237", "Larry Ellison");
 		eMap.put("a1238", "Bill Gates");
-	    return eMap.get("A1234".toLowerCase());
+	    return eMap.get(k.toLowerCase());
 	}
 		
 }
--- list-map/src/test/java/com/mitrais/cdc/java/EmployeeMapTest.java
+++ list-map/src/test/java/com/mitrais/cdc/java/EmployeeMapTest.java
@@ -12,7 +12,7 @@ public class EmployeeMapTest {
 	public void test() {
 		String[] stringArray = {"Steve Jobs", "Scott McNealy", "Jeff Bezos", "Larry Ellison","Bill Gates"};
 		EmployeeMap employee = new EmployeeMap();
-		String name = employee.runEmployeeMap();
+		String name = employee.runEmployeeMap("a1234");
 		assertNotNull(name);
 		assertTrue(Arrays.asList(stringArray).contains(name));
 	}
