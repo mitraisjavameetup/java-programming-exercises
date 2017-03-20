86e87326099741d572e90110c2e1fe767a5b5d31
trainee <trainee>
2017-03-20 17:26:46 +0700

    hafieludin yusuf rizana junit-exercise

--- junit-exercise/src/main/java/com/mitrais/cdc/java/App.java
+++ junit-exercise/src/main/java/com/mitrais/cdc/java/App.java
@@ -1,5 +1,7 @@
 package com.mitrais.cdc.java;
 
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 /**
@@ -14,9 +16,12 @@ public class App {
 	   *  removed. For example, Utils.calmString("!foo!!") should return "foo".
 	   */
 	  public static String calmString(String speech) {
+		  String calmSpeech = "";
 		//todo: apply logic as described above
-
-	    return(speech);
+		  for(int i=0;i<speech.length();i++){
+			  if(speech.charAt(i)!='!')calmSpeech=new StringBuilder().append(calmSpeech).append(speech.charAt(i)).toString();
+		  }
+	    return calmSpeech;
 	  }
 	  
 	  /** Method calmAttendees: 
@@ -26,9 +31,36 @@ public class App {
 	   *  and half time it remove "Democrat"
 	   *  If it contains neither,or is null, it does nothing.
 	   */
+	  private static boolean checker(List<String> attendingParties){
+		  for(String at:attendingParties){
+			  if(attendingParties.contains("Republican")||attendingParties.contains("Democrat"))return true;
+		  }
+		  return false;
+	  }
 	  public static void calmAttendees(List<String> attendingParties) {
 			//todo: apply logic as described above
-	  }
+		  if(!attendingParties.isEmpty()){
+			  boolean decide=checker(attendingParties);
+			  if(decide){
+				  double decider=Math.random();
+				  if(decider<=0.5){
+					  for(int i=0;i<attendingParties.size();i++){
+						  if(attendingParties.get(i).contains("Republican"))
+						  attendingParties.remove(i);
+					  }
+				  }
+				  else{
+						  for(int i=0;i<attendingParties.size();i++){
+							  if(attendingParties.get(i).contains("Democrat"))
+							  attendingParties.remove(i);
+						  }
+					  }
+				  }
+			  }
+			  
+		  }
+		  
+	  
 	  
 	  private App() {} // Class cannot be instantiated: only static methods
 	}
