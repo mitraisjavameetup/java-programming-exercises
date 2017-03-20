22c78f1383501b1f955a988dd4960c8392279f92
trainee <trainee>
2017-03-20 11:53:14 +0700

    NAIM ARYUDYA AZIZI list-map

--- list-map/src/main/java/com/mitrais/cdc/java/NumberMap.java
+++ list-map/src/main/java/com/mitrais/cdc/java/NumberMap.java
@@ -1,5 +1,6 @@
 package com.mitrais.cdc.java;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class NumberMap {
@@ -17,11 +18,19 @@ public class NumberMap {
 
 	  public NumberMap(String[] words) {
 		// To-Do set numberTable map by words arrays 
+		  numberTable = new HashMap<Integer, String>();
+		  for (int i = 0; i < words.length; i++) {
+			  numberTable.put(i, words[i]);
+		}
 	  }
 
 	  public String wordForNumber(int num) {
 		// To-Do return words by numbers assigned, set default as [unknown]
-	    return null;
+	    if (numberTable.containsKey(num)) {
+			return numberTable.get(num);
+		} else {
+			return "[unknown]";
+		}
 	  }
 
 	  public int getSize() {
