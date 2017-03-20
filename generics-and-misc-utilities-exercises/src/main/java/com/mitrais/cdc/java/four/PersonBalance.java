07d6fa4b187414c34739e3e6414f16885e9ec776
trainee <trainee>
2017-03-20 14:57:23 +0700

    hafieludin yusuf rizana generics-and-misc-utilities-exercises with javadoc

--- generics-and-misc-utilities-exercises/src/main/java/com/mitrais/cdc/java/four/PersonBalance.java
+++ generics-and-misc-utilities-exercises/src/main/java/com/mitrais/cdc/java/four/PersonBalance.java
@@ -7,8 +7,48 @@ public class PersonBalance {
 	//todo:
 	//- create constructor
 	//- create setter/getter for the attributes/properties
-
+	/**\
+	 * 
+	 * @param name
+	 * @param balance
+	 */
+	public PersonBalance(String name,double balance) {
+	this.name=name;
+	this.balance=balance;
+	}
+	/**
+	 * get name
+	 * @return String
+	 */
+	public String getName() {
+		return name;
+	}
+	/**
+	 * set name
+	 * @param name
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	/**
+	 * get balance
+	 * @return double
+	 */
+	public double getBalance() {
+		return balance;
+	}
+	/**
+	 * set balance
+	 * @param balance
+	 */
+	public void setBalance(double balance) {
+		this.balance = balance;
+	}
+	/**
+	 * print out person's bank account
+	 */
 	public void printThis() {
+		System.out.printf("%7s's bank account balance is $%,12.2f.\n",name,balance );
 		//Assume that the names are 7 characters or fewer
 		//and the balances are less than $10M
 		//TODO: create value that lined up like this
--- generics-and-misc-utilities-exercises/src/main/java/com/mitrais/cdc/java/three/TwoItems.java
+++ generics-and-misc-utilities-exercises/src/main/java/com/mitrais/cdc/java/three/TwoItems.java
@@ -2,18 +2,55 @@ package com.mitrais.cdc.java.three;
 
 public class TwoItems<T> {
 	//TODO: define two generic type here
-
-	//TODO: define parameterized constructor here
-
-	//TODO: define getter for those two items defined above
+	private T item1,item2;
 	
+	/**
+	 * 
+	 * @param item1
+	 * @param item2
+	 */
+	//TODO: define parameterized constructor here
+	public TwoItems(T item1,T item2){
+		this.item1=item1;
+		this.item2=item2;
+	}
+	//TODO: define getter for those two items defined above
+	/**
+	 * get item1
+	 * @return item1
+	 */
+	public T getItem1() {
+		return item1;
+	}
+	/**
+	 * set item1
+	 * @param item1
+	 */
+	public void setItem1(T item1) {
+		this.item1 = item1;
+	}
+	/**
+	 * get item2
+	 * @return item2
+	 */
+	public T getItem2() {
+		return item2;
+	}
+	/**
+	 * set item2
+	 * @param item2
+	 */
+	public void setItem2(T item2) {
+		this.item2 = item2;
+	}
 	//TODO: override toString method here
+	/**
+	 * overrided toString() method
+	 */
 	@Override
 	public String toString(){
 		String returnString = new String();
-		
-		
-		
+		returnString=new StringBuilder().append("item1=").append(item1).append(" item2=").append(item2).toString();
 		return returnString;
 	}
 }
