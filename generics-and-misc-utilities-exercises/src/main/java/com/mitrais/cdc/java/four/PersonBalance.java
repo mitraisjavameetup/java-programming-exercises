0b850c8164d095cbe76d2ba4ba94d34437a88e23
trainee <trainee>
2017-03-20 14:52:56 +0700

    20/3/2017 generics exercise

--- generics-and-misc-utilities-exercises/src/main/java/com/mitrais/cdc/java/four/PersonBalance.java
+++ generics-and-misc-utilities-exercises/src/main/java/com/mitrais/cdc/java/four/PersonBalance.java
@@ -3,17 +3,22 @@ package com.mitrais.cdc.java.four;
 public class PersonBalance {
 	private String name;
 	private double balance;
-	
-	//todo:
-	//- create constructor
-	//- create setter/getter for the attributes/properties
+
+	// todo:
+	// - create constructor
+	// - create setter/getter for the attributes/properties
+
+	public PersonBalance(String name, double balance) {
+		this.name = name;
+		this.balance = balance;
+	}
 
 	public void printThis() {
-		//Assume that the names are 7 characters or fewer
-		//and the balances are less than $10M
-		//TODO: create value that lined up like this
-		//Juanita's bank account balance is $2,345,678.99.
-		//   Juan's bank account balance is $   15,455.26.
-		
+		// Assume that the names are 7 characters or fewer
+		// and the balances are less than $10M
+		// TODO: create value that lined up like this
+		// Juanita's bank account balance is $2,345,678.99.
+		// Juan's bank account balance is $ 15,455.26.
+		System.out.println(String.format("%1$7s's bank account balance is $%2$,12.2f.", name, balance));
 	}
 }
--- generics-and-misc-utilities-exercises/src/main/java/com/mitrais/cdc/java/three/TwoItems.java
+++ generics-and-misc-utilities-exercises/src/main/java/com/mitrais/cdc/java/three/TwoItems.java
@@ -2,17 +2,30 @@ package com.mitrais.cdc.java.three;
 
 public class TwoItems<T> {
 	//TODO: define two generic type here
-
-	//TODO: define parameterized constructor here
-
-	//TODO: define getter for those two items defined above
+	private T item1;
+	private T item2;
 	
+	//TODO: define parameterized constructor here
+	public TwoItems(T item1, T item2) {
+		this.item1 = item1;
+		this.item2 = item2;
+	}
+	
+	//TODO: define getter for those two items defined above
+	public T getItem1() {
+		return item1;
+	}
+
+	public T getItem2() {
+		return item2;
+	}
+
 	//TODO: override toString method here
 	@Override
 	public String toString(){
 		String returnString = new String();
 		
-		
+		returnString = getItem1()+", "+getItem2();
 		
 		return returnString;
 	}
