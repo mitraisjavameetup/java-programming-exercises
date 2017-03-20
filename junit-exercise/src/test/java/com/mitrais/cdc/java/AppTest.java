422dc9317b7854f8e708ee581b6014068eb0eff2
trainee <trainee>
2017-03-20 18:12:46 +0800

    Muhammad Ramziadh Afgeshza

--- junit-exercise/src/test/java/com/mitrais/cdc/java/AppTest.java
+++ junit-exercise/src/test/java/com/mitrais/cdc/java/AppTest.java
@@ -1,10 +1,23 @@
 package com.mitrais.cdc.java;
 
+import static org.junit.Assert.*;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.*;
+
 /**
  * Unit test for simple App.
  */
 public class AppTest{
 
+	@Test
+	/**
+	 * Test for CalmString
+	 */
 	public void testCalmString()
     {
         /* todo:
@@ -13,8 +26,21 @@ public class AppTest{
          * at least create 4 test cases using 'assertThat' (modern approach)
          * and 4 test cases using 'assertEquals' (traditional approach)
          */
+		assertThat(App.calmString("egi!!"), is(equalTo("egi")));
+		assertThat(App.calmString("!!egi!!"), is(equalTo("egi")));
+		assertThat(App.calmString("!!e!!gi!!"), is(equalTo("egi")));
+		assertThat(App.calmString("!!e!!g!!i!!"), is(equalTo("egi")));
+		
+		assertEquals("egi", App.calmString("egi!"));
+		assertEquals("egi", App.calmString("!egi!"));
+		assertEquals("egi", App.calmString("!e!gi!"));
+		assertEquals("egi", App.calmString("!e!g!i!"));
     }
 	
+	@Test
+	/**
+	 * Test for CalmString
+	 */
 	public void testCalmAttendees()
     {
         /* todo:
@@ -22,5 +48,19 @@ public class AppTest{
          * to test calmAttendees method on App.java
          * at least create 3 test case using 'assertThat'
          */
+		String[] _attendingParties1 = {"Republican of People's Democrat", "Liberalism"};
+		String[] _attendingParties2 = {"National Democrat", "Dream Republican"};
+	    List<String> attendingParties1 = Arrays.asList(_attendingParties1);
+	    List<String> attendingParties2 = Arrays.asList(_attendingParties2);
+	    int listSize1 = attendingParties1.size();
+	    int listSize2 = attendingParties2.size();
+	    
+	    App.calmAttendees(attendingParties1);
+	    App.calmAttendees(attendingParties2);
+	    
+		assertThat(attendingParties1, is(notNullValue()));
+		assertThat(attendingParties1.size(), is(equalTo(listSize1)));
+		assertThat(attendingParties2, is(notNullValue()));
+		assertThat(attendingParties2.size(), is(equalTo(listSize2)));
     }
 }
