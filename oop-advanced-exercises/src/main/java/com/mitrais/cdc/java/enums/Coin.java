6dc4e3cabc958abc24a616c05c17d9fde186e642
trainee <trainee>
2017-03-20 09:44:32 +0700

    oop advanced exercise

--- oop-advanced-exercises/src/main/java/com/mitrais/cdc/java/enums/Coin.java
+++ oop-advanced-exercises/src/main/java/com/mitrais/cdc/java/enums/Coin.java
@@ -1,12 +1,31 @@
 package com.mitrais.cdc.java.enums;
+/**
+ * 
+ * @author Agus Mistiawan
+ * @version 1.0.0
+ *
+ */
+import java.util.Random;
 
 public enum Coin {
     HEADS, TAILS;
-	
+	/**
+	 * Random value, 0 is HEADS and 1 is TAILS
+	 * @return enum(HEADS,TAILS)
+	 */
 	public static Coin flip() {
-
 	  //TODO: Create calculation to return head or tail with equal probability
-	  //TODO: later, create main class to call this method (e.g. looped 10 times) 
-	  return Coin.HEADS; //replace this
+	  //TODO: later, create main class to call this method (e.g. looped 10 times)
+		Random r = new Random();
+		int random = r.nextInt((1-0)+1)+0;
+		if(random<0.5)
+			return Coin.HEADS; //replace this
+		else
+			return Coin.TAILS;
   }
+	
+	public static void main(String[] args) {
+		for(int i=0; i<10; i++)
+			System.out.println(Coin.flip());
+	}
 }
--- oop-advanced-exercises/src/main/java/com/mitrais/cdc/java/resources/PaperclipSale.java
+++ oop-advanced-exercises/src/main/java/com/mitrais/cdc/java/resources/PaperclipSale.java
@@ -1,5 +1,10 @@
 package com.mitrais.cdc.java.resources;
-
+/**
+ * 
+ * @author Agus Mistiawan
+ * @version 1.0.0
+ *
+ */
 public class PaperclipSale implements Sellable {
 
     private String color;
@@ -8,24 +13,10 @@ public class PaperclipSale implements Sellable {
 	
   public PaperclipSale(String color, double boxPrice, int numBoxes) {
 	//TODO: complete this so it will set the properties when called   
-
-  
-  
-  
+	  this.color = color;
+	  this.boxPrice = boxPrice;
+	  this.numBoxes = numBoxes;
   }
-
-  //TODO: define getter each attributes/properties
-
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
 
   @Override
   /**
@@ -33,24 +24,41 @@ public class PaperclipSale implements Sellable {
    */
   public double getCost() {
 	double finalCost = 0;
-		
-	//TODO: create calculation to calculate finalCost based on price and number of boxes here
-
-	
-	
-	
-	
+	finalCost = boxPrice * numBoxes;
 	return(finalCost);  	  
   }
+  /**
+   *  will return type of color
+   *  @return color
+   */
+  public String getColor() {
+	return color;
+  }
+  
+  /**
+   * Will return Price per box
+   * @return boxPrice
+   */
 
-  @Override
+  public double getBoxPrice() {
+	return boxPrice;
+  }	
+  /**
+   * Will return number of boxes
+   * @return numBoxes
+   */
+  public int getNumBoxes() {
+	return numBoxes;
+}
+
+  /**
+   * Will return String of value the instance
+   */
+@Override
   public String toString() {
 	String toString = new String();
-	
 	//TODO: modify this method to return sentence e.g. "This paperclip name is blablabla, its price is blabla, with number of box blabla"
-	
-	  
-	  
+	System.out.println("This paperclip name is "+color+", its price is "+boxPrice+", with number of box "+numBoxes);
 	return toString;  
   }
 }
