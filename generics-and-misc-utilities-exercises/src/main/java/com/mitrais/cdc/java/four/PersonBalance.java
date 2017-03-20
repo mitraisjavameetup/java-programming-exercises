8abcb273f1b84fe273a9d80d18455e46c77a6b34
trainee <trainee>
2017-03-20 15:23:32 +0700

    Edward Chadnra - Generic and Util Training Exercise

--- generics-and-misc-utilities-exercises/src/main/java/com/mitrais/cdc/java/four/PersonBalance.java
+++ generics-and-misc-utilities-exercises/src/main/java/com/mitrais/cdc/java/four/PersonBalance.java
@@ -4,16 +4,40 @@ public class PersonBalance {
 	private String name;
 	private double balance;
 	
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getBalance() {
+		return balance;
+	}
+
+	public void setBalance(double balance) {
+		this.balance = balance;
+	}
+	
 	//todo:
 	//- create constructor
 	//- create setter/getter for the attributes/properties
 
+	PersonBalance(String name,double balance)
+	{
+		this.name=name;
+		this.balance=balance;
+	}
+	
 	public void printThis() {
 		//Assume that the names are 7 characters or fewer
 		//and the balances are less than $10M
 		//TODO: create value that lined up like this
 		//Juanita's bank account balance is $2,345,678.99.
 		//   Juan's bank account balance is $   15,455.26.
+		System.out.printf("%7s's bank account balance is $%,12.2f.%n",getName(),getBalance());
 		
 	}
 }
