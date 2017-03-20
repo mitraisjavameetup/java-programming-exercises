6db7a963d34b1aff49de68ac18df3557d0b8a61e
trainee <trainee>
2017-03-20 09:34:06 +0700

    Krisnanjaya

--- oop-advanced-exercises/src/main/java/com/mitrais/cdc/java/enums/Coin.java
+++ oop-advanced-exercises/src/main/java/com/mitrais/cdc/java/enums/Coin.java
@@ -4,9 +4,21 @@ public enum Coin {
     HEADS, TAILS;
 	
 	public static Coin flip() {
-
+		
+		double temp = Math.random();
+		if(temp < 0.5){
+			return Coin.HEADS;
+		}
+		else{
+			return Coin.TAILS;
+		}
 	  //TODO: Create calculation to return head or tail with equal probability
 	  //TODO: later, create main class to call this method (e.g. looped 10 times) 
-	  return Coin.HEADS; //replace this
+	  
   }
+	public static void main(String[] args) {
+		for(int i=0; i<10; i++){
+			System.out.println(Coin.flip());
+		}
+	}
 }
--- oop-advanced-exercises/src/main/java/com/mitrais/cdc/java/resources/PaperclipSale.java
+++ oop-advanced-exercises/src/main/java/com/mitrais/cdc/java/resources/PaperclipSale.java
@@ -8,24 +8,13 @@ public class PaperclipSale implements Sellable {
 	
   public PaperclipSale(String color, double boxPrice, int numBoxes) {
 	//TODO: complete this so it will set the properties when called   
-
-  
-  
-  
+	  this.color = color;
+	  this.boxPrice = boxPrice;
+	  this.numBoxes = numBoxes;
   }
 
   //TODO: define getter each attributes/properties
-
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
+ 
 
   @Override
   /**
@@ -35,20 +24,31 @@ public class PaperclipSale implements Sellable {
 	double finalCost = 0;
 		
 	//TODO: create calculation to calculate finalCost based on price and number of boxes here
-
-	
-	
-	
+	finalCost = this.numBoxes * this.boxPrice;
 	
 	return(finalCost);  	  
   }
 
-  @Override
+  public String getColor() {
+	return color;
+}
+
+public double getBoxPrice() {
+	return boxPrice;
+}
+
+
+public int getNumBoxes() {
+	return numBoxes;
+}
+
+
+@Override
   public String toString() {
 	String toString = new String();
 	
 	//TODO: modify this method to return sentence e.g. "This paperclip name is blablabla, its price is blabla, with number of box blabla"
-	
+	toString = "This paperclip Color is "+getColor()+", its price is "+getBoxPrice()+", with number of box "+getNumBoxes();
 	  
 	  
 	return toString;  
