a44715357072c345a6e2e96c96adda2337de9560
trainee <trainee>
2017-03-20 12:40:42 +0700

    Steven Indra - List Map Exercise

---
+++ list-map/src/main/java/com/mitrais/cdc/java/EmployeeMapTest2.java
@@ -0,0 +1,12 @@
+package com.mitrais.cdc.java;
+
+public class EmployeeMapTest2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		EmployeeMap employee = new EmployeeMap();
+		String name = employee.runEmployeeMap("A1235");
+		System.out.println(name);
+	}
+
+}
