f652a4178ea344f70c7ce31e7e5540c585ad9f2b
trainee <trainee>
2017-03-21 09:31:09 +0700

    NAIM ARYUDYA AZIZI simple-graphics-exercises

--- simple-graphics-exercises/src/main/java/com/mitrais/cdc/java/coreservlets/GatesPanel.java
+++ simple-graphics-exercises/src/main/java/com/mitrais/cdc/java/coreservlets/GatesPanel.java
@@ -5,17 +5,19 @@ import javax.swing.*;
 
 public class GatesPanel extends JPanel {
   private Image image;
-  
+  protected ImageIcon i;
   public GatesPanel(String relativePath) {
 	//get image from relativePath, and store it into variable
-
-  
+	  image = Toolkit.getDefaultToolkit().getImage(relativePath);
+	  i = new ImageIcon(image);
+	  i.getIconWidth();
   }
 
   @Override
   protected void paintComponent(Graphics g) {
     super.paintComponent(g);
     g.drawImage(image, 0, 0, this);
+    g.drawLine(470, 217, 500, 217);
     
     //add mustache under here. PS: use simple line as his mustache
 
