dd71aff8bd0f309a432268ad18e618b92831fcca
trainee <trainee>
2017-03-21 15:25:32 +0700

    hafieludin yusuf rizana multithreaded-programming-exercise

--- multithreaded-programming-exercises/src/main/java/com/mitrais/cdc/java/one/CoinFlipper.java
+++ multithreaded-programming-exercises/src/main/java/com/mitrais/cdc/java/one/CoinFlipper.java
@@ -1,15 +1,37 @@
 package com.mitrais.cdc.java.one;
 
+/**
+ * 
+ * @author trainee
+ * coinFlipper Class
+ */
 public class CoinFlipper implements Runnable {
+	/**
+	 * method to flip coin
+	 */
   @Override
   public void run() {
+	  
 	//TODO: create 'for' loop 1000 times
 	//and also place counter in it.
 	//each time number in counter >= 3, print something like this:
 	//"Pool-1-thread-1 got 3 heads in a row!!"
 	//or like this"
 	//"Pool-1-thread-2 got 5 heads in a row!!"  
-
+	  int counter=0;
+	  boolean head=true;
+	  for(int i=0;i<1000;i++){
+		  double decider=Math.random();
+		  if(decider<=0.5){
+			  if(head)counter+=1;
+			  else {head=true;}
+		  }
+		  else{
+			  
+			  counter=0;head=false;
+		  }
+		  if(counter>=3)System.out.println(Thread.currentThread().getName()+" got "+counter+" heads in a row!!");
+	  }
 
 	  
 	  
--- multithreaded-programming-exercises/src/main/java/com/mitrais/cdc/java/three/FlipCoins3.java
+++ multithreaded-programming-exercises/src/main/java/com/mitrais/cdc/java/three/FlipCoins3.java
@@ -2,8 +2,15 @@ package com.mitrais.cdc.java.three;
 import java.awt.*;
 import java.util.concurrent.*;
 import javax.swing.*;
-
+/**
+ * 
+ * @author trainee
+ * FlipCoins3 Class
+ */
 public class FlipCoins3 extends JFrame {
+	/**
+	 * FlipCoins3 constructor
+	 */
   public FlipCoins3() {
     super("Flipping Coins");
     getContentPane().setLayout(new GridLayout(5, 1));
@@ -11,16 +18,14 @@ public class FlipCoins3 extends JFrame {
     int poolSize = 10;
     
     //TODO: Instantiate ExcecutorService class with poolsize as stated above
-
+    ExecutorService taskList=Executors.newFixedThreadPool(poolSize);
     
     for(int i=0; i<5; i++) {
       JLabel label = new JLabel();
       label.setFont(new Font("SansSerif", Font.PLAIN, 60));
       
       //TODO: add line to instantiate labelling coin flipper and execute it.
-
-      
-      
+      taskList.execute(new LabelingCoinFlipper(label));
       add(label);
     }
     setSize(400, 400);
