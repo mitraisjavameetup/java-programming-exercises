12728cb8f8504539990f42498c9d933b5a8081f2
trainee <trainee>
2017-03-21 10:12:56 +0700

    gui java

--- simple-graphics-exercises/src/main/java/com/mitrais/cdc/java/coreservlets/TicTacToeFrame2.java
+++ simple-graphics-exercises/src/main/java/com/mitrais/cdc/java/coreservlets/TicTacToeFrame2.java
@@ -1,21 +1,23 @@
 package com.mitrais.cdc.java.coreservlets;
 
+import java.awt.Color;
+
 import javax.swing.*;
 
 public class TicTacToeFrame2 extends JFrame {
   public TicTacToeFrame2() {
     super("Tic-Tac-Toe");
     //complete this method to show tic tac toe board
-
-  
-  
-  
+    setContentPane(new TicTacToePanel2(Color.GRAY));
+    setSize(620,640);
+    setResizable(false);
+    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    setLocationRelativeTo(null);
+    setVisible(true);
   
   }
   
   public static void main(String[] args) {
-	//call method to show tic tac toe frame
-
-  
+	  new TicTacToeFrame2();
   }
 }
--- simple-graphics-exercises/src/main/java/com/mitrais/cdc/java/coreservlets/TicTacToePanel2.java
+++ simple-graphics-exercises/src/main/java/com/mitrais/cdc/java/coreservlets/TicTacToePanel2.java
@@ -4,12 +4,26 @@ import java.awt.*;
 import javax.swing.*;
 
 public class TicTacToePanel2 extends JPanel {
-  @Override
+  public TicTacToePanel2(Color color) {
+		// TODO Auto-generated constructor stub
+	  setBackground(color);
+	}
+
+@Override
   protected void paintComponent(Graphics g) {
     super.paintComponent(g);
     
     //add code to show lines, so it look like tic tac toe board
-    
+    /*
+     * Horizontal Line
+     */
+    g.drawLine(0, getWidth()/3, getWidth(), getWidth()/3);
+    g.drawLine(0, (getWidth()*2)/3, getWidth(), (getWidth()*2)/3);
+    /*
+     * Vertical Line
+     */
+    g.drawLine(getHeight()/3, 0, getHeight()/3, getHeight());
+    g.drawLine((getHeight()*2)/3, 0, (getHeight()*2)/3, getHeight());
     
     
     
