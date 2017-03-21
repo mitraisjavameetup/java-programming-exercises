0051c5c3721fd8ae7371a65a31d6afd0c75f8c04
trainee <trainee>
2017-03-21 10:10:22 +0700

    Bery Orindi

--- simple-graphics-exercises/src/main/java/com/mitrais/cdc/java/coreservlets/GatesFrame.java
+++ simple-graphics-exercises/src/main/java/com/mitrais/cdc/java/coreservlets/GatesFrame.java
@@ -1,21 +1,26 @@
 package com.mitrais.cdc.java.coreservlets;
 
+import java.awt.Color;
+
 import javax.swing.*;
 
 public class GatesFrame extends JFrame {
+	/** GatesFrame for */
   public GatesFrame(String relativePath) {
     super("Bill Gates");
     //complete the method to show image
-
-  
-  
+    GatesPanel gp = new GatesPanel(relativePath);
+    setContentPane(gp);
+    setSize(gp.img.getIconWidth(), gp.img.getIconHeight());  
+    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    setVisible(true); 
   
   }
-  
+  /** main function*/
   public static void main(String[] args) {
 	//call method to show image on frame
 	//image location is on images/gates-blue-screen.jpg
-
-  
+	 new GatesFrame("images/gates-blue-screen.jpg");
+	  
   }
 }
