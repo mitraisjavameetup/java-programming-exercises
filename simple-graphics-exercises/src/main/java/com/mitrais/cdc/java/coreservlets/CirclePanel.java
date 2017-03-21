da352959b5428294b4979f034dffc689d799d1d0
trainee <trainee>
2017-03-21 09:39:57 +0700

    Add simple-graphics-exercises

--- simple-graphics-exercises/src/main/java/com/mitrais/cdc/java/coreservlets/CirclePanel.java
+++ simple-graphics-exercises/src/main/java/com/mitrais/cdc/java/coreservlets/CirclePanel.java
@@ -1,6 +1,7 @@
 package com.mitrais.cdc.java.coreservlets;
 
 import java.awt.*;
+import java.util.Random;
 
 import javax.swing.*;
 
@@ -15,7 +16,14 @@ public class CirclePanel extends JPanel {
   protected void paintComponent(Graphics g) {
     super.paintComponent(g);
     //complete this method to display yellow rounds on screen
-    
+    for (int i = 0; i < 50; i++) {
+    	g.setColor(Color.YELLOW);
+    	Random ran = new Random();
+    	int x = ran.nextInt(this.getWidth()-10);
+    	int y = ran.nextInt(this.getHeight()-10);
+		g.fillOval(x,y, 10, 10);
+		
+	}
     
     
     
--- simple-graphics-exercises/src/main/java/com/mitrais/cdc/java/coreservlets/TicTacToePanel1.java
+++ simple-graphics-exercises/src/main/java/com/mitrais/cdc/java/coreservlets/TicTacToePanel1.java
@@ -8,9 +8,10 @@ public class TicTacToePanel1 extends JPanel {
   protected void paintComponent(Graphics g) {
     super.paintComponent(g);
     //add code to show lines, so it look like tic tac toe board
-
-  
-  
+    g.drawLine(0, 100, 300, 100);
+    g.drawLine(0, 200, 300, 200);
+    g.drawLine(100, 0, 100, 300);
+    g.drawLine(200, 0, 200, 300);
   
   }
 }
