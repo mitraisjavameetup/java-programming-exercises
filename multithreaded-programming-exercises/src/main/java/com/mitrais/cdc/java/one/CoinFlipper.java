259515ac9e5e41bed461cbadbc64e1fcd59ce6b1
agriardyan <aagriard@gmail.com>
2017-03-21 15:18:26 +0700

    done multithreaded-programming-exercise

--- multithreaded-programming-exercises/src/main/java/com/mitrais/cdc/java/one/CoinFlipper.java
+++ multithreaded-programming-exercises/src/main/java/com/mitrais/cdc/java/one/CoinFlipper.java
@@ -1,25 +1,29 @@
 package com.mitrais.cdc.java.one;
 
 public class CoinFlipper implements Runnable {
-  @Override
-  public void run() {
-	//TODO: create 'for' loop 1000 times
-	//and also place counter in it.
-	//each time number in counter >= 3, print something like this:
-	//"Pool-1-thread-1 got 3 heads in a row!!"
-	//or like this"
-	//"Pool-1-thread-2 got 5 heads in a row!!"  
 
+	private int consecutive;
 
-	  
-	  
-	  
-	  
-	  
-	  
-	  
-	  
-	  
-	  
-  }
+	@Override
+	public void run() {
+		// TODO: create 'for' loop 1000 times
+		// and also place counter in it.
+		// each time number in counter >= 3, print something like this:
+		// "Pool-1-thread-1 got 3 heads in a row!!"
+		// or like this"
+		// "Pool-1-thread-2 got 5 heads in a row!!"
+		int rand = 0;
+		for (int i = 0; i < 1000; i++) {
+			rand = ((int) (Math.random() * 10));
+			if(rand % 2 == 0) {
+				consecutive++;
+			} else {
+				if(consecutive >= 3) {
+					System.out.println(Thread.currentThread().getName() + " has " + consecutive + " head in a row");
+				}
+				
+				consecutive = 0;
+			}
+		}
+	}
 }
--- multithreaded-programming-exercises/src/main/java/com/mitrais/cdc/java/three/LabelingCoinFlipper.java
+++ multithreaded-programming-exercises/src/main/java/com/mitrais/cdc/java/three/LabelingCoinFlipper.java
@@ -1,39 +1,52 @@
 package com.mitrais.cdc.java.three;
+
 import javax.swing.*;
 
 public class LabelingCoinFlipper implements Runnable {
-  private JLabel label;
+	private JLabel label;
+	private int consecutive;
 
-  public LabelingCoinFlipper(JLabel label) {
-    label.setText(String.format("Heads: %3d", 0));
-    this.label = label;
-  }
+	public LabelingCoinFlipper(JLabel label) {
+		label.setText(String.format("Heads: %3d", 0));
+		this.label = label;
+	}
 
-  @Override
-  public void run() {
-    pause(200);
-    
-    //TODO: create 'for' loop 1000 times
-  	//and also place counter in it.
-  	//Print something like this:
-  	//"Heads: 1!!"
-  	//or like this"
-  	//"Heads: 3"  
-    //do not forget to add "pause(5);" after change the label text
-    //so you could see the entire process
-    
-    
-    
-    
-    
-    
-    
-    
-  }
+	@Override
+	public void run() {
+		// TODO: create 'for' loop 1000 times
+		// and also place counter in it.
+		// Print something like this:
+		// "Heads: 1!!"
+		// or like this"
+		// "Heads: 3"
+		// do not forget to add "pause(5);" after change the label text
+		// so you could see the entire process
+		int rand = 0;
+		for (int i = 0; i < 1000; i++) {
+			rand = ((int) (Math.random() * 10));
+			if (rand % 2 == 0) {
+				consecutive++;
+			} else {
+				if (consecutive >= 3) {
+					String str = String
+							.format(Thread.currentThread().getName() + " has " + consecutive + " head in a row");
+					this.label.setText(str);
 
-  private static void pause(int millis) {
-    try {
-      Thread.sleep(millis);
-    } catch(InterruptedException ie) {}
-  }
+				}
+
+				consecutive = 0;
+			}
+
+			pause(20);
+
+		}
+
+	}
+
+	private static void pause(int millis) {
+		try {
+			Thread.sleep(millis);
+		} catch (InterruptedException ie) {
+		}
+	}
 }
