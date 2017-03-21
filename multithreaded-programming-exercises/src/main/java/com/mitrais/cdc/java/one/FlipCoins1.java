ac3f59e3f6d67849ea01a60c7a33fd91d5f5f828
trainee <trainee>
2017-03-21 15:30:14 +0700

    Edward Chandra - MultiThreading Programming

--- multithreaded-programming-exercises/src/main/java/com/mitrais/cdc/java/one/FlipCoins1.java
+++ multithreaded-programming-exercises/src/main/java/com/mitrais/cdc/java/one/FlipCoins1.java
@@ -5,17 +5,18 @@ public class FlipCoins1 {
   public static void main(String[] args) {
     int poolSize = 10;
     
-    
     //TODO: Instantiate ExcecutorService class with poolsize as stated above
+    ExecutorService tasks= Executors.newFixedThreadPool(poolSize);
     
     
     //TODO execute the CoinFlipper 5 times (or several times) concurrently 
-
-    
+    for (int i=0;i<5;i++)
+    {
+    tasks.execute(new CoinFlipper());
+    }
     
     
     //TODO: do not forget to shutdown the executor
-    
-    
+    tasks.shutdown();
   }
 }
