829d67d7b597d931136c60e6c3f31b7846c373b7
trainee <trainee>
2017-03-21 15:22:23 +0800

    Satriya Wicaksana multithread-exercise

--- multithreaded-programming-exercises/src/main/java/com/mitrais/cdc/java/one/CoinFlipper.java
+++ multithreaded-programming-exercises/src/main/java/com/mitrais/cdc/java/one/CoinFlipper.java
@@ -1,25 +1,38 @@
 package com.mitrais.cdc.java.one;
 
 public class CoinFlipper implements Runnable {
-  @Override
-  public void run() {
-	//TODO: create 'for' loop 1000 times
-	//and also place counter in it.
-	//each time number in counter >= 3, print something like this:
-	//"Pool-1-thread-1 got 3 heads in a row!!"
-	//or like this"
-	//"Pool-1-thread-2 got 5 heads in a row!!"  
-
-
-	  
-	  
-	  
-	  
-	  
-	  
-	  
-	  
-	  
-	  
-  }
+	/** override function run, flip coin and count face in a row */
+	@Override
+	public void run() {
+		//TODO: create 'for' loop 1000 times
+		//and also place counter in it.
+		//each time number in counter >= 3, print something like this:
+		//"Pool-1-thread-1 got 3 heads in a row!!"
+		//or like this"
+		//"Pool-1-thread-2 got 5 heads in a row!!"
+		int counter = 0;
+		boolean heads = false;
+		for(int i=0;i<1000;i++){
+			if(Math.random() >= 0.5){
+				if(!heads){
+					counter = 0;
+					heads=true;
+				}
+				counter++;
+				if(counter >= 3){
+					System.out.printf("%s got %d heads in a row!!%n", Thread.currentThread().getName(),counter);
+				}
+			}
+			else{
+				if(heads){
+					counter = 0;
+					heads = false;
+				}
+				counter++;
+				if(counter >= 3){
+					System.out.printf("%s got %d tails in a row!!%n", Thread.currentThread().getName(),counter);
+				}
+			}
+		}
+	}
 }
--- multithreaded-programming-exercises/src/main/java/com/mitrais/cdc/java/two/FlipCoins2.java
+++ multithreaded-programming-exercises/src/main/java/com/mitrais/cdc/java/two/FlipCoins2.java
@@ -5,55 +5,57 @@ import java.util.concurrent.*;
  */
 
 public class FlipCoins2 implements Runnable {
-  
-  public FlipCoins2() {
-	// Note that you cannot do tasks.execute(this) directly from main.
-	// When you are in main, no instance of the surrounding class has
-	// yet been created, so there *IS* no "this". So, here main calls
-	// the constructor of the class, and that constructor does tasks.execute(this).
-	  
-	// Similarly, you cannot make
-	// an instance of an inner class directly from main. Again, this is because
-    // in main, the outer class has not yet been built.
-	  
-    int poolSize = 10;
-    
-    //TODO: Instantiate ExcecutorService class with poolsize as stated above
 
-    
-    
-    //TODO execute this 5 times (or several times) concurrently
-    
-    
-    
-    
-    
-    //TODO: do not forget to shutdown the executor
-    
-    
-  }
+	public FlipCoins2() {
+		// Note that you cannot do tasks.execute(this) directly from main.
+		// When you are in main, no instance of the surrounding class has
+		// yet been created, so there *IS* no "this". So, here main calls
+		// the constructor of the class, and that constructor does tasks.execute(this).
 
-  
-  
- public static void main(String[] args) {
-   new FlipCoins2();
- }
- 
- 
- 
-  @Override
-  public void run() {
-	//TODO: Just copy run() method from question number 1 :)
-
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  }
+		// Similarly, you cannot make
+		// an instance of an inner class directly from main. Again, this is because
+		// in main, the outer class has not yet been built.
+		int poolSize = 10;
+		//TODO: Instantiate ExcecutorService class with poolsize as stated above
+		ExecutorService taskList = Executors.newFixedThreadPool(poolSize);
+		//TODO execute this 5 times (or several times) concurrently
+		taskList.execute(this);
+		taskList.execute(this);
+		taskList.execute(this);
+		taskList.execute(this);
+		taskList.execute(this);
+		//TODO: do not forget to shutdown the executor
+		taskList.shutdown();
+	}
+	public static void main(String[] args) {
+		new FlipCoins2();
+	}
+	@Override
+	public void run() {
+		//TODO: Just copy run() method from question number 1 :)
+		int counter = 0;
+		boolean heads = false;
+		for(int i=0;i<1000;i++){
+			if(Math.random() >= 0.5){
+				if(!heads){
+					counter = 0;
+					heads=true;
+				}
+				counter++;
+				if(counter >= 3){
+					System.out.printf("%s got %d heads in a row!!%n", Thread.currentThread().getName(),counter);
+				}
+			}
+			else{
+				if(heads){
+					counter = 0;
+					heads = false;
+				}
+				counter++;
+				if(counter >= 3){
+					System.out.printf("%s got %d tails in a row!!%n", Thread.currentThread().getName(),counter);
+				}
+			}
+		}
+	}
 }
