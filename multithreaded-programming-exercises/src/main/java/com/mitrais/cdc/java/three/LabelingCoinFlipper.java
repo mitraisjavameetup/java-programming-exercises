7df79d69423d53dbb7513ea40e35a2c13dbc0a24
trainee <trainee>
2017-03-21 15:42:34 +0700

    multithread Irvan

--- multithreaded-programming-exercises/src/main/java/com/mitrais/cdc/java/three/LabelingCoinFlipper.java
+++ multithreaded-programming-exercises/src/main/java/com/mitrais/cdc/java/three/LabelingCoinFlipper.java
@@ -1,39 +1,42 @@
 package com.mitrais.cdc.java.three;
+
 import javax.swing.*;
 
 public class LabelingCoinFlipper implements Runnable {
-  private JLabel label;
+	private JLabel label;
 
-  public LabelingCoinFlipper(JLabel label) {
-    label.setText(String.format("Heads: %3d", 0));
-    this.label = label;
-  }
+	public LabelingCoinFlipper(JLabel label) {
+		label.setText(String.format("Heads: %3d", 0));
+		this.label = label;
+	}
 
-  @Override
-  public void run() {
-    pause(200);
-    
-    //TODO: create 'for' loop 1000 times
-  	//and also place counter in it.
-  	//Print something like this:
-  	//"Heads: 1!!"
-  	//or like this"
-  	//"Heads: 3"  
-    //do not forget to add "pause(5);" after change the label text
-    //so you could see the entire process
-    
-    
-    
-    
-    
-    
-    
-    
-  }
+	@Override
+	public void run() {
+		int counterHead = 0;
+		for (int i = 0; i < 1000; i++) {
+			if (Math.random() <= 0.5) {
+				counterHead++;
+				label.setText(String.format("Heads: %3d", counterHead));
+				pause(5);
+			} else {
+				pause(5);
+			}
+		}
+		// TODO: create 'for' loop 1000 times
+		// and also place counter in it.
+		// Print something like this:
+		// "Heads: 1!!"
+		// or like this"
+		// "Heads: 3"
+		// do not forget to add "pause(5);" after change the label text
+		// so you could see the entire process
 
-  private static void pause(int millis) {
-    try {
-      Thread.sleep(millis);
-    } catch(InterruptedException ie) {}
-  }
+	}
+
+	private static void pause(int millis) {
+		try {
+			Thread.sleep(millis);
+		} catch (InterruptedException ie) {
+		}
+	}
 }
--- multithreaded-programming-exercises/src/main/java/com/mitrais/cdc/java/two/FlipCoins2.java
+++ multithreaded-programming-exercises/src/main/java/com/mitrais/cdc/java/two/FlipCoins2.java
@@ -1,59 +1,69 @@
 package com.mitrais.cdc.java.two;
+
 import java.util.concurrent.*;
 
-/** Second variation: main class implements Runnable.
+/**
+ * Second variation: main class implements Runnable.
  */
 
 public class FlipCoins2 implements Runnable {
-  
-  public FlipCoins2() {
-	// Note that you cannot do tasks.execute(this) directly from main.
-	// When you are in main, no instance of the surrounding class has
-	// yet been created, so there *IS* no "this". So, here main calls
-	// the constructor of the class, and that constructor does tasks.execute(this).
-	  
-	// Similarly, you cannot make
-	// an instance of an inner class directly from main. Again, this is because
-    // in main, the outer class has not yet been built.
-	  
-    int poolSize = 10;
-    
-    //TODO: Instantiate ExcecutorService class with poolsize as stated above
 
-    
-    
-    //TODO execute this 5 times (or several times) concurrently
-    
-    
-    
-    
-    
-    //TODO: do not forget to shutdown the executor
-    
-    
-  }
+	public FlipCoins2() {
+		// Note that you cannot do tasks.execute(this) directly from main.
+		// When you are in main, no instance of the surrounding class has
+		// yet been created, so there *IS* no "this". So, here main calls
+		// the constructor of the class, and that constructor does
+		// tasks.execute(this).
 
-  
-  
- public static void main(String[] args) {
-   new FlipCoins2();
- }
- 
- 
- 
-  @Override
-  public void run() {
-	//TODO: Just copy run() method from question number 1 :)
+		// Similarly, you cannot make
+		// an instance of an inner class directly from main. Again, this is
+		// because
+		// in main, the outer class has not yet been built.
 
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  }
+		int poolSize = 10;
+		ExecutorService tasks = Executors.newFixedThreadPool(10);
+		for (int i = 0; i < 5; i++) {
+			tasks.execute(this);
+		}
+		tasks.shutdown();
+
+		// TODO: Instantiate ExcecutorService class with poolsize as stated
+		// above
+
+		// TODO execute this 5 times (or several times) concurrently
+
+		// TODO: do not forget to shutdown the executor
+
+	}
+
+	public static void main(String[] args) {
+		new FlipCoins2();
+	}
+
+	@Override
+	public void run() {
+		// TODO: Just copy run() method from question number 1
+		int counterHead = 0;
+		int counterTail = 0;
+		for (int i = 0; i < 1000; i++) {
+			if (Math.random() <= 0.5) {
+				counterHead++;
+				counterTail = 0;
+			} else {
+				counterTail++;
+				counterHead = 0;
+			}
+
+			if (counterHead >= 3) {
+				System.out.println(Thread.currentThread().getName() + " got " + counterHead
+						+ " heads in a row");
+
+			} else if (counterTail >= 3) {
+				System.out.println(Thread.currentThread().getName() + " got " + counterTail
+						+ " tails in a row");
+
+			}
+		}
+
+	}
 }
