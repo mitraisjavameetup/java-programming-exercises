70d5da9527fd2868f295d73cad8e5534cd9f5f95
DGFrance <donnygfrance@gmail.com>
2017-03-22 13:48:36 +0700

    Donnyv1.0.6

--- parallel/src/main/java/com/mitrais/cdc/java/MathUtils.java
+++ parallel/src/main/java/com/mitrais/cdc/java/MathUtils.java
@@ -1,24 +1,31 @@
 package com.mitrais.cdc.java;
 
+import java.security.cert.TrustAnchor;
 import java.util.*;
 import java.util.concurrent.*;
 
+import javax.print.attribute.standard.PrinterMessageFromOperator;
+
 public class MathUtils {
   private static final ForkJoinPool FORK_JOIN_POOL = new ForkJoinPool();
+  
   
   /** Make an ordinary (serial) method that, given a boolean[], will loop down the array and mark each
    * entry as true or false, depending on whether that index is prime. For example, array[0] and array[1]
    * should be false (0 and 1 are not prime), array[2] and array[3] should be true (2 and 3 are prime),
    * array[4] should be false (4 is not prime), array[5] should be true (5 is prime), and so forth.
    * Notes:
-   * • Use Primes.isPrime(number) to test whether a number is prime. This method is already built in to
+   * ï¿½ Use Primes.isPrime(number) to test whether a number is prime. This method is already built in to
    * the Primes class that you copied from the fork-join-exercises project.
-   * • To simplify the later parallel version, break your code into two methods, one that takes the whole
+   * ï¿½ To simplify the later parallel version, break your code into two methods, one that takes the whole
    * array and one that takes the array and two indices.
    */
-  public static void markPrimesSerial(boolean[] primeFlags, 
-                                      int lowerIndex, int upperIndex) {
+  public static void markPrimesSerial(boolean[] primeFlags, int lowerIndex, int upperIndex) {
 	  // To Do : One simple line of code that uses Primes.isPrime
+	  for (int i= lowerIndex; i <= upperIndex; i++)
+		  primeFlags[i] = Primes.isPrime(i); 
+	  
+	
   }
   
   /** Marks each entry i with true or false depending on
@@ -26,7 +33,9 @@ public class MathUtils {
    *  marks all entries in the array.
    */
   public static void markPrimesSerial(boolean[] primeFlags) {
-    markPrimesSerial(primeFlags, 0, primeFlags.length-1);
+	  
+			markPrimesSerial(primeFlags, 0, primeFlags.length -1);
+    
   }
   
   /** Marks each entry i with true or false depending on
@@ -35,6 +44,7 @@ public class MathUtils {
    */
   public static void markPrimesParallel(boolean[] primeFlags) {
 	  // To Do : create parallel version of mark primes
+	 FORK_JOIN_POOL.invoke(new ParallelPrimeMarker(primeFlags, 0 , primeFlags.length -1));
   }
   
   /** Given an already-marked boolean[] of flags that says
@@ -43,7 +53,15 @@ public class MathUtils {
    */
   public static List<Integer> collectPrimes(boolean[] primeFlags) {
 	  // To Do : collect and return list of marked prime numbers from array
-	  return null;
+	  List<Integer> primesNum = new ArrayList<Integer>();
+		 // for (int i= lowerindex; i < )
+	  for (int i= 0 ; i < primeFlags.length ; i++ ){
+		  if(primeFlags[i] == true) {
+		  primesNum.add(i);
+		  }
+		  
+	  }
+	  return primesNum;
   }
   
   /** Given an unmarked boolean[] of flags, serially marks each one saying
@@ -56,7 +74,16 @@ public class MathUtils {
   
   public static List<Integer> findPrimesSerial(boolean[] primeFlags) {
 	// To Do : collect and return list of marked prime numbers given the empty boolean array
-   return null;
+	TimingUtils.timeOp(new Op() {
+		
+		@Override
+		public String runOp() {
+			// TODO Auto-generated method stub
+			return "Serial Time";
+		}
+	});
+	  
+   return collectPrimes(primeFlags);
   }
   
   /** Uses findPrimesSerial(booleanArray) to produce a List of primes
@@ -64,7 +91,10 @@ public class MathUtils {
   
   public static List<Integer> findPrimesSerial(int size) {
 	// To Do : collect and return list of marked prime numbers from given only the size
-    return null;
+	  boolean[] primeFlags = new boolean[size];
+	  List<Integer> primesNum = new ArrayList<Integer>();
+	  primesNum = findPrimesSerial(primeFlags);
+    return primesNum;
   }
   
   /** Given an unmarked boolean[] of flags, marks each one in parallel, saying
@@ -75,9 +105,18 @@ public class MathUtils {
    *  from the time of marking it.
    */
   
-  public static List<Integer> findPrimesParallel(boolean[] primeFlags) {
+  public static List<Integer> findPrimesParallel(final boolean[] primeFlags) {
 	// To Do : collect and return list of marked prime numbers given the empty boolean array
-    return null;
+	 TimingUtils.timeOp(new Op() {
+		
+		@Override
+		public String runOp() {
+			markPrimesParallel(primeFlags);
+			// TODO Auto-generated method stub
+			return "Parallel Time";
+		}
+	});
+	 return collectPrimes(primeFlags);
   }
   
   /** Uses findPrimesParallel(booleanArray) to produce a List of primes
@@ -85,7 +124,11 @@ public class MathUtils {
   
   public static List<Integer> findPrimesParallel(int size) {
     // To Do : collect and return list of marked prime numbers from given only the size
-    return null;
+	  boolean[]primeFlags = new boolean[size];
+	  List<Integer> primesNum = new ArrayList<Integer>();
+	  primesNum = findPrimesParallel(primeFlags);
+	  
+    return collectPrimes(primeFlags);
   }
   
 }
