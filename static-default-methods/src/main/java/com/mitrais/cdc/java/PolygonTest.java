e58c7f196bd8e0f9ac4ddfab279276b703042b0a
trainee <trainee>
2017-04-11 15:18:36 +0700

    satriya wicaksana exercise static and default method

--- static-default-methods/src/main/java/com/mitrais/cdc/java/PolygonTest.java
+++ static-default-methods/src/main/java/com/mitrais/cdc/java/PolygonTest.java
@@ -8,5 +8,7 @@ public class PolygonTest {
 	 * Create Polygon using EquilateralTriangle and Square
 	 * then print out the totalsides  
 	 */
+	  RegularPolygon[] rp = {new Square(10), new EquilateralTriangle(8)};
+	  System.out.println("Sum of area: " + RegularPolygon.totalSides(rp));
   }
 }
