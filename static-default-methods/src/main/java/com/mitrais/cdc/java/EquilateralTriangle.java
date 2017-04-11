e6dc425f876727d78f4fbcb14bf5757808519c81
Ali Firdaus Ghifari <alifirdausghifari@gmail.com>
2017-04-11 15:19:53 +0700

    implements all abstracts methods from RegularPolygon interface

--- static-default-methods/src/main/java/com/mitrais/cdc/java/EquilateralTriangle.java
+++ static-default-methods/src/main/java/com/mitrais/cdc/java/EquilateralTriangle.java
@@ -1,9 +1,29 @@
 package com.mitrais.cdc.java;
 
 public class EquilateralTriangle implements RegularPolygon {
+
 	/* To Do :
 	 * Create a class EquilateralTriangle that implements the interface, 
 	 *     has getNumSides return 3 and getSideLength return an instance variable that is set by the constructor.
 	 */
+	private double length;
+	
+	@Override
+	public int getNumSides() {
+		// TODO Auto-generated method stub
+		return 3;
+	}
+
+	@Override
+	public double getSideLength() {
+		// TODO Auto-generated method stub
+		return this.length;
+	}
+
+	public EquilateralTriangle(double length) {
+		super();
+		this.length = length;
+	}
+	
 	
 }
--- static-default-methods/src/main/java/com/mitrais/cdc/java/Square.java
+++ static-default-methods/src/main/java/com/mitrais/cdc/java/Square.java
@@ -1,10 +1,29 @@
 package com.mitrais.cdc.java;
 
 public class Square implements RegularPolygon {
+
 	
 	/* To Do :
 	 * Create a class EquilateralTriangle that implements the interface, 
 	 *     has getNumSides return 4 and getSideLength return an instance variable that is set by the constructor.
 	 */
+	private double length;
+	
+	@Override
+	public int getNumSides() {
+		// TODO Auto-generated method stub
+		return 4;
+	}
+
+	@Override
+	public double getSideLength() {
+		// TODO Auto-generated method stub
+		return this.length;
+	}
+
+	public Square(double length) {
+		super();
+		this.length = length;
+	}
 	
 }
