14e1826cadf02514e90102b70dd243881eb84dc0
agriardyan <superbro.ardyan@gmail.com>
2017-04-11 14:13:51 +0700

    done lambda-3-exercise

--- lambdas-3-exercise/src/main/java/com/mitrais/cdc/java/ElementUtils.java
+++ lambdas-3-exercise/src/main/java/com/mitrais/cdc/java/ElementUtils.java
@@ -3,35 +3,37 @@ package com.mitrais.cdc.java;
 import java.util.*;
 import java.util.function.*;
 
-/** Some generically-typed versions of methods that transform Lists. See
- *  StringUtils for the less flexible String-specific versions.
+/**
+ * Some generically-typed versions of methods that transform Lists. See
+ * StringUtils for the less flexible String-specific versions.
  */
 
 public class ElementUtils {
-  public static <T> List<T> allMatches(List<T> candidates, Predicate<T> matchFunction) {
-    
-	List<T> results = new ArrayList<>();
-    
-	//TODO: Question number two: return a new List of all the values that passed the test
-    
+    public static <T> List<T> allMatches(List<T> candidates, Predicate<T> matchFunction) {
 
-	
-	
-	
-    return(results); 
-  }
+        List<T> results = new ArrayList<>();
 
-  public static <T, R> List<R> transformedList(List<T> originals, Function<T, R> transformer) {
-    List<R> results = new ArrayList<>();
-    
-    //TODO: Question number four: return a new List that contains the results of applying the Function to each element of the original
+        //TODO: Question number two: return a new List of all the values that passed the test
+        for (T c : candidates) {
+            if (matchFunction.test(c)) {
+                results.add(c);
+            }
+        }
 
-    
-    
-    
-    
-    return(results); 
-  }
-  
-  private ElementUtils() {} // Uninstantiatable class
+        return (results);
+    }
+
+    public static <T, R> List<R> transformedList(List<T> originals, Function<T, R> transformer) {
+        List<R> results = new ArrayList<>();
+
+        //TODO: Question number four: return a new List that contains the results of applying the Function to each element of the original
+        for (T c : originals) {
+            results.add(transformer.apply(c));
+        }
+
+        return (results);
+    }
+
+    private ElementUtils() {
+    } // Uninstantiatable class
 }
--- lambdas-3-exercise/src/main/java/com/mitrais/cdc/java/StringUtils.java
+++ lambdas-3-exercise/src/main/java/com/mitrais/cdc/java/StringUtils.java
@@ -3,40 +3,39 @@ package com.mitrais.cdc.java;
 import java.util.*;
 import java.util.function.*;
 
-/** Some String-specific versions of methods that transform Lists. See
- *  ElementUtils for the better, generically-typed versions.
+/**
+ * Some String-specific versions of methods that transform Lists. See
+ * ElementUtils for the better, generically-typed versions.
  */
 
 public class StringUtils {
-  public static List<String> allMatches(List<String> candidates, Predicate<String> matchFunction) {
-	
-    List<String> results = new ArrayList<>();
-    
-    //TODO: Question number one: return a new List of all the values that passed the test
+    public static List<String> allMatches(List<String> candidates, Predicate<String> matchFunction) {
 
-    
-    
-    
-    
-    
-    
-    return(results); 
-  }
-  
-  
-  public static List<String> transformedList(List<String> originals, Function<String, String> transformer) {
-    
-	List<String> results = new ArrayList<>();
-    
-    //TODO: Question number three: return a new List that contains the results of applying the Function to each element of the original
+        List<String> results = new ArrayList<>();
 
-	
-	
-	
-    
-    
-    return(results); 
-  }
-  
-  private StringUtils() {} // Uninstantiatable class
+        //TODO: Question number one: return a new List of all the values that passed the test
+        for (String c : candidates) {
+            if(matchFunction.test(c)) {
+                results.add(c);
+            }
+        }
+
+        return (results);
+    }
+
+
+    public static List<String> transformedList(List<String> originals, Function<String, String> transformer) {
+
+        List<String> results = new ArrayList<>();
+
+        //TODO: Question number three: return a new List that contains the results of applying the Function to each element of the original
+        for(String x : originals) {
+            results.add(transformer.apply(x));
+        }
+
+        return (results);
+    }
+
+    private StringUtils() {
+    } // Uninstantiatable class
 }
