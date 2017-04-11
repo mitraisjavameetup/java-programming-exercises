e93832fc541cacb6f50612116b5469b1c4d1ab33
trainee <trainee>
2017-04-11 13:45:41 +0700

    tuesday 11 apr 2017 lambda 3 exercise

--- lambdas-3-exercise/src/main/java/com/mitrais/cdc/java/ElementUtils.java
+++ lambdas-3-exercise/src/main/java/com/mitrais/cdc/java/ElementUtils.java
@@ -13,12 +13,13 @@ public class ElementUtils {
 	List<T> results = new ArrayList<>();
     
 	//TODO: Question number two: return a new List of all the values that passed the test
+	for (T candidate : candidates) {
+    	if (matchFunction.test(candidate)) {
+    		results.add(candidate);
+    	}
+    }
     
-
-	
-	
-	
-    return(results); 
+    return results;
   }
 
   public static <T, R> List<R> transformedList(List<T> originals, Function<T, R> transformer) {
@@ -26,11 +27,11 @@ public class ElementUtils {
     
     //TODO: Question number four: return a new List that contains the results of applying the Function to each element of the original
 
+    for (T original : originals) {
+		results.add(transformer.apply(original));
+    }
     
-    
-    
-    
-    return(results); 
+    return results;
   }
   
   private ElementUtils() {} // Uninstantiatable class
