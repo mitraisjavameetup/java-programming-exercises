e70f81f7b9d9f752cb9fd529c450189e11515c16
trainee <trainee>
2017-04-11 14:45:33 +0700

    lambda part 3 by jac

--- lambdas-3-exercise/src/main/java/com/mitrais/cdc/java/ElementUtils.java
+++ lambdas-3-exercise/src/main/java/com/mitrais/cdc/java/ElementUtils.java
@@ -31,7 +31,10 @@ public class ElementUtils {
     List<R> results = new ArrayList<>();
     
     //TODO: Question number four: return a new List that contains the results of applying the Function to each element of the original
-
+    for (int i = 0; i < results.size(); i++) {
+		results.add(transformer.apply(originals.get(i)));
+	}
+    
     
     
     
