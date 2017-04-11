a316c0c8cef998cef44a045d97ca4538aca47144
trainee <trainee>
2017-04-11 16:59:57 +0700

    Bery Orindi

--- lambdas-4-exercises/src/main/java/com/mitrais/cdc/java/three/FunctionUtils2.java
+++ lambdas-4-exercises/src/main/java/com/mitrais/cdc/java/three/FunctionUtils2.java
@@ -8,19 +8,14 @@ public class FunctionUtils2 {
   @SafeVarargs
   public static <T> Predicate<T> anyPassPredicate(Predicate<T>... tests) {
     Predicate<T> result = e -> false;
-
-    
     /** TODO: create a 'for' loop. 
      * 	Returns a Predicate that is the result of ORing all the argument Predicates.
      *  If no Predicates are supplied, it returns a Predicate that always returns
      *  false.
      */
-    
-
-    
-    
-    
-    
+    for(Predicate<T> test: tests){
+    	result = result.or(test);
+    }  
     return(result);
   }
   
@@ -33,10 +28,10 @@ public class FunctionUtils2 {
     T result = null;
     
     //TODO: filter based first element that matches ANY of the tests, null otherwise
-    
-    
-    
-    
+    result = elements
+    		.filter(combinedTest)
+            .findFirst()
+            .orElse(null);
     
     return result;
   }
