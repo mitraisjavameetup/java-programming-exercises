b1d411404ec4eb659c227b5f755d134e00e59837
trainee <trainee>
2017-04-11 17:05:19 +0700

    sebelum pulang

--- lambdas-4-exercises/src/main/java/com/mitrais/cdc/java/three/FunctionUtils2.java
+++ lambdas-4-exercises/src/main/java/com/mitrais/cdc/java/three/FunctionUtils2.java
@@ -15,6 +15,9 @@ public class FunctionUtils2 {
      *  If no Predicates are supplied, it returns a Predicate that always returns
      *  false.
      */
+    for (Predicate<T> predicate : tests) {
+		result.or(predicate);
+	}
     
 
     
@@ -33,7 +36,10 @@ public class FunctionUtils2 {
     T result = null;
     
     //TODO: filter based first element that matches ANY of the tests, null otherwise
-    
+    result = elements
+    		.filter(combinedTest)
+    		.findFirst()
+    		.orElse(null);
     
     
     
