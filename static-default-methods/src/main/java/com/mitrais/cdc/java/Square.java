20427713b80817d7525f7f2be71cb0e35106c2ff
trainee <trainee>
2017-04-11 15:31:57 +0700

    static-exercise

--- static-default-methods/src/main/java/com/mitrais/cdc/java/Square.java
+++ static-default-methods/src/main/java/com/mitrais/cdc/java/Square.java
@@ -1,10 +1,29 @@
 package com.mitrais.cdc.java;
 
 public class Square implements RegularPolygon {
+	private double sideLength = 4;
+	
+	public Square() {
+		// TODO Auto-generated constructor stub
+	}
+		
+	public Square(double sideLength) {
+		this.sideLength = sideLength;
+	}
 	
 	/* To Do :
 	 * Create a class EquilateralTriangle that implements the interface, 
 	 *     has getNumSides return 4 and getSideLength return an instance variable that is set by the constructor.
 	 */
+	@Override
+	public int getNumSides() {
+		// TODO Auto-generated method stub
+		return 4;
+	}
 	
+	@Override
+	public double getSideLength() {
+		// TODO Auto-generated method stub
+		return sideLength;
+	}
 }
