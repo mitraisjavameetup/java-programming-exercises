89dc97efe42c09488dbaddb90da82f495b5cc7c5
trainee <trainee>
2017-04-11 15:37:40 +0700

    static default methods

--- static-default-methods/src/main/java/com/mitrais/cdc/java/EquilateralTriangle.java
+++ static-default-methods/src/main/java/com/mitrais/cdc/java/EquilateralTriangle.java
@@ -5,5 +5,24 @@ public class EquilateralTriangle implements RegularPolygon {
 	 * Create a class EquilateralTriangle that implements the interface, 
 	 *     has getNumSides return 3 and getSideLength return an instance variable that is set by the constructor.
 	 */
+	public double sideLength;
+	
+	public EquilateralTriangle(double sideLength) {
+		// TODO Auto-generated constructor stub
+		this.sideLength = sideLength;
+	}
+
+	public int getNumSides() {
+		// TODO Auto-generated method stub
+		return 3;
+	}
+
+	public double getSideLength() {
+		// TODO Auto-generated method stub
+		return this.sideLength;
+	}
+
+
+
 	
 }
--- static-default-methods/src/main/java/com/mitrais/cdc/java/PolygonTest.java
+++ static-default-methods/src/main/java/com/mitrais/cdc/java/PolygonTest.java
@@ -1,12 +1,17 @@
 package com.mitrais.cdc.java;
 
-import java.util.*;
-
 public class PolygonTest {
   public static void main(String[] args) {
 	/*
 	 * Create Polygon using EquilateralTriangle and Square
 	 * then print out the totalsides  
 	 */
+	  RegularPolygon[] reg = {
+			  new EquilateralTriangle(5),
+			  new Square(5)
+	  };
+	  
+	  
+	  System.out.println(RegularPolygon.totalSides(reg));
   }
 }
