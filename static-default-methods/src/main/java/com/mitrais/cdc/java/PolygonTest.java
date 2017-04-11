6365cb7795d316daf7459915d498c25391dd7533
trainee <trainee>
2017-04-11 15:28:20 +0700

    hafieludin yusuf rizana static-default-methods with javadoc

--- static-default-methods/src/main/java/com/mitrais/cdc/java/PolygonTest.java
+++ static-default-methods/src/main/java/com/mitrais/cdc/java/PolygonTest.java
@@ -8,5 +8,10 @@ public class PolygonTest {
 	 * Create Polygon using EquilateralTriangle and Square
 	 * then print out the totalsides  
 	 */
+	  RegularPolygon[] polygon={
+			  new EquilateralTriangle(10),
+			  new Square(10)
+	  };
+	  System.out.println("Sum :"+RegularPolygon.totalSides(polygon));
   }
 }
--- static-default-methods/src/main/java/com/mitrais/cdc/java/RegularPolygon.java
+++ static-default-methods/src/main/java/com/mitrais/cdc/java/RegularPolygon.java
@@ -9,5 +9,25 @@ public interface RegularPolygon {
    *    • getPerimeter (n * length, where n is the number of sides)
    *    • getInteriorAngle ( (n-2)π/n in radians)
    */
- 
+	int getNumSides();
+	double getSideLength();
+	/**
+	 * method to calculate totalSides
+	 * @param regularPolygon
+	 * @return int
+	 */
+	public static int totalSides(RegularPolygon[] regularPolygon){
+		int sum=0;
+		for(RegularPolygon r:regularPolygon){
+			sum=sum+r.getNumSides();
+		}
+		return sum;
+	}
+	default double getPerimeter(){
+		return (getNumSides()*getSideLength());
+	}
+	default double getInteriorAngle(){
+		return (getNumSides()-2)*Math.PI/getNumSides();
+	}
+	
 }
