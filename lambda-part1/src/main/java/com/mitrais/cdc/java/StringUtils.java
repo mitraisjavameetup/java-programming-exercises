b0fae425b81ba865d7c1447e8accd83e0218368d
agriardyan <superbro.ardyan@gmail.com>
2017-04-11 10:47:14 +0700

    done exercise lambda-part1

--- lambda-part1/src/main/java/com/mitrais/cdc/java/StringUtils.java
+++ lambda-part1/src/main/java/com/mitrais/cdc/java/StringUtils.java
@@ -10,20 +10,20 @@ public class StringUtils {
 		 * That method will take 2 strings and an instance of your interface.
 		 * It returns string1 if the method in interface returns true, string2 otherwise. 
 		 * This method is normal Java 7 code.
-		 */	
-		
-		return null;
+		 */
+
+		return tester.isBetter(s1, s2) ? s1 : s2;
 	}
-	
+
 	public static int eChecker(String s1, String s2) {
-	    int compareFlag = 0;
-	    if (s1.contains("e") && !s2.contains("e")) {
-	      compareFlag = -1;
-	    } else if (s2.contains("e") && !s1.contains("e")) {
-	      compareFlag = 1;
-	    }
-	    return(compareFlag);
-	  }
+		int compareFlag = 0;
+		if (s1.contains("e") && !s2.contains("e")) {
+			compareFlag = -1;
+		} else if (s2.contains("e") && !s1.contains("e")) {
+			compareFlag = 1;
+		}
+		return(compareFlag);
+	}
 
 	private StringUtils() {}
 
