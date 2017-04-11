9ae19fbd32ac5c961430f487d801c0c5799771ab
trainee <trainee>
2017-04-11 11:02:46 +0700

    Lamba part 1

--- lambda-part1/src/main/java/com/mitrais/cdc/java/SortingExamples.java
+++ lambda-part1/src/main/java/com/mitrais/cdc/java/SortingExamples.java
@@ -1,26 +1,30 @@
 package com.mitrais.cdc.java;
 
-public class SortingExamples {
+import java.util.Arrays;
 
+public class SortingExamples {
+	  
   // array containing a few Strings
-  public static String[] words = { "hi", "hello", "hola", "bye", "goodbye", "adios" };
+  public static String[] words = { "hello", "hola", "hi", "bye", "goodbye", "adios" };
+
   
   public static String[] sortedByLengthAscending(){
 	  String[] sortedByLengthAscending = words;
-	  
+	  Arrays.sort(sortedByLengthAscending, (s1,s2) -> (s1.length() - s2.length()));
 	  /*
 	   * TO DO
 	   * Sort words by length (i.e., shortest to longest)
 	   * 
 	   */
-	  
+
 	  return sortedByLengthAscending;
+
   }
   
   
   public static String[] sortedByLengthDescending(){
 	  String[] sortedByLengthDescending = words;
-	  
+	  Arrays.sort(sortedByLengthDescending, (s1,s2) -> (s2.length() - s1.length()));
 	  /*
 	   * TO DO
 	   * Sort words by reverse length (i.e., longest to shortest)
@@ -33,20 +37,21 @@ public class SortingExamples {
   
   public static String[] sortedByFirstLetter(){
 	  String[] sortedByFirstLetter = words;
-	  
+	  Arrays.sort(sortedByFirstLetter, (s1,s2) -> (s1.charAt(0) - s2.charAt(0)));
 	  /*
 	   * TO DO
 	   * Sort words alphabetically by the first character only
 	   * 
 	   */
-	  
+
 	  return sortedByFirstLetter;
 			  
   }
   
   public static String[] sortedByContains(){
 	  String[] sortedByContains = words;
-	  
+	  Arrays.sort(sortedByContains, StringUtils::eChecker);
+//	  Arrays.sort(sortedByContains, (s1,s2) -> StringUtils.eChecker(s1, s2));
 	  /*
 	   * TO DO
 	   * Sort words by whether it contains 'e'
@@ -54,7 +59,7 @@ public class SortingExamples {
 	   * Hint: use method StringUtils.eChecker
 	   * 
 	   */
-	  
+
 	  return sortedByContains;
 			  
   } 
