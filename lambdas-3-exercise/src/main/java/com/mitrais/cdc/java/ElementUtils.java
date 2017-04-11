7734c79d1986b7a43b71f8b55ed1ed505b69e950
trainee <trainee>
2017-04-11 13:35:52 +0700

    hafieludin yusuf rizana lambdas 3 exercise

--- lambdas-3-exercise/src/main/java/com/mitrais/cdc/java/ElementUtils.java
+++ lambdas-3-exercise/src/main/java/com/mitrais/cdc/java/ElementUtils.java
@@ -13,10 +13,11 @@ public class ElementUtils {
 	List<T> results = new ArrayList<>();
     
 	//TODO: Question number two: return a new List of all the values that passed the test
-    
-
-	
-	
+    for(T c:candidates){
+    	if(matchFunction.test(c)){
+    		results.add(c);
+    	}
+    }
 	
     return(results); 
   }
@@ -25,7 +26,9 @@ public class ElementUtils {
     List<R> results = new ArrayList<>();
     
     //TODO: Question number four: return a new List that contains the results of applying the Function to each element of the original
-
+    for(T o:originals){
+    	results.add(transformer.apply(o)); 
+    }
     
     
     
