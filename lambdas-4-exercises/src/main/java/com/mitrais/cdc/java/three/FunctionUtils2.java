7416bbd9ed54a1342e37d8808d05ba27247da9d8
Ali Firdaus Ghifari <alifirdausghifari@gmail.com>
2017-04-11 16:50:04 +0700

    filtering word with predicate and streams

--- lambdas-4-exercises/src/main/java/com/mitrais/cdc/java/three/FunctionUtils2.java
+++ lambdas-4-exercises/src/main/java/com/mitrais/cdc/java/three/FunctionUtils2.java
@@ -4,42 +4,37 @@ import java.util.function.Predicate;
 import java.util.stream.Stream;
 
 public class FunctionUtils2 {
-	
-  @SafeVarargs
-  public static <T> Predicate<T> anyPassPredicate(Predicate<T>... tests) {
-    Predicate<T> result = e -> false;
 
-    
-    /** TODO: create a 'for' loop. 
-     * 	Returns a Predicate that is the result of ORing all the argument Predicates.
-     *  If no Predicates are supplied, it returns a Predicate that always returns
-     *  false.
-     */
-    
+	@SafeVarargs
+	public static <T> Predicate<T> anyPassPredicate(Predicate<T>... tests) {
+		Predicate<T> result = e -> false;
 
-    
-    
-    
-    
-    return(result);
-  }
-  
-  
-  
-  @SafeVarargs
-  public static <T> T firstAnyMatch(Stream<T> elements, Predicate<T>... tests) {
-    Predicate<T> combinedTest = anyPassPredicate(tests);
-    
-    T result = null;
-    
-    //TODO: filter based first element that matches ANY of the tests, null otherwise
-    
-    
-    
-    
-    
-    return result;
-  }
-  
-  private FunctionUtils2() {} // Uninstantiatable class
+		/**
+		 * TODO: create a 'for' loop. Returns a Predicate that is the result of
+		 * ORing all the argument Predicates. If no Predicates are supplied, it
+		 * returns a Predicate that always returns false.
+		 */
+		for (Predicate<T> test : tests) {
+			result = result.or(test);
+		}
+
+		return (result);
+	}
+
+	@SafeVarargs
+	public static <T> T firstAnyMatch(Stream<T> elements, Predicate<T>... tests) {
+		Predicate<T> combinedTest = anyPassPredicate(tests);
+
+		T result = null;
+
+		// TODO: filter based first element that matches ANY of the tests, null
+		// otherwise
+
+		result = elements.filter(combinedTest).findFirst().orElse(null);
+
+		return result;
+	}
+
+	private FunctionUtils2() {
+	} // Uninstantiatable class
 }
