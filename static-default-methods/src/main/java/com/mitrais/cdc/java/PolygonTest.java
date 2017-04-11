1ce463966352425cea94354775386da707b0770b
Ali Firdaus Ghifari <alifirdausghifari@gmail.com>
2017-04-11 15:21:27 +0700

    test with system.out.println to see the totalsides result

--- static-default-methods/src/main/java/com/mitrais/cdc/java/PolygonTest.java
+++ static-default-methods/src/main/java/com/mitrais/cdc/java/PolygonTest.java
@@ -8,5 +8,12 @@ public class PolygonTest {
 	 * Create Polygon using EquilateralTriangle and Square
 	 * then print out the totalsides  
 	 */
+	  
+	 RegularPolygon[] regularPolygons = {
+			 new EquilateralTriangle(5),
+			 new Square(4)
+	 };
+	 
+	 System.out.println(RegularPolygon.totalSides(regularPolygons));
   }
 }
