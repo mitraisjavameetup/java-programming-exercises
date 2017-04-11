0a9f27533757e38d905b66c6f226d8d2e89cf600
trainee <trainee>
2017-04-11 15:28:28 +0700

    Bery Orindi

--- static-default-methods/src/main/java/com/mitrais/cdc/java/EquilateralTriangle.java
+++ static-default-methods/src/main/java/com/mitrais/cdc/java/EquilateralTriangle.java
@@ -5,5 +5,26 @@ public class EquilateralTriangle implements RegularPolygon {
 	 * Create a class EquilateralTriangle that implements the interface, 
 	 *     has getNumSides return 3 and getSideLength return an instance variable that is set by the constructor.
 	 */
+	public double length; 
+	
+	
+	public EquilateralTriangle(double length) {
+		super();
+		this.length = length;
+	}
+
+
+	public int getNumSides(){
+		return 3;
+	}
+	
+	public void setLength(double length) {
+		this.length = length;
+	}
+
+	public double getSideLength(){
+		return this.length;
+	}
+	
 	
 }
--- static-default-methods/src/main/java/com/mitrais/cdc/java/Square.java
+++ static-default-methods/src/main/java/com/mitrais/cdc/java/Square.java
@@ -6,5 +6,23 @@ public class Square implements RegularPolygon {
 	 * Create a class EquilateralTriangle that implements the interface, 
 	 *     has getNumSides return 4 and getSideLength return an instance variable that is set by the constructor.
 	 */
+	public double length; 
+	
+	public Square(double length) {
+		super();
+		this.length = length;
+	}
+
+	public int getNumSides(){
+		return 4;
+	}
+
+	public void setLength(double length) {
+		this.length = length;
+	}
+
+	public double getSideLength(){
+		return this.length;
+	}
 	
 }
