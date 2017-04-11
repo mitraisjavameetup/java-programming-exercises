b520df5ea781a24daa108199fe085a94df2765eb
trainee <trainee>
2017-04-11 15:15:21 +0700

    Edward Chandra - static and default method

--- static-default-methods/src/main/java/com/mitrais/cdc/java/EquilateralTriangle.java
+++ static-default-methods/src/main/java/com/mitrais/cdc/java/EquilateralTriangle.java
@@ -1,9 +1,26 @@
 package com.mitrais.cdc.java;
 
 public class EquilateralTriangle implements RegularPolygon {
+
 	/* To Do :
 	 * Create a class EquilateralTriangle that implements the interface, 
 	 *     has getNumSides return 3 and getSideLength return an instance variable that is set by the constructor.
 	 */
+	
+	private double sideLength;
+	
+	EquilateralTriangle(double sideLength){
+		this.sideLength=sideLength;
+	}
+	
+	public int getNumSides() {
+		// TODO Auto-generated method stub
+		return 3;
+	}
+
+	public double getSideLength() {
+		// TODO Auto-generated method stub
+		return this.sideLength;
+	}
 	
 }
