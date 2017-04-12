a39cec7169a5124b3ee07360b30c780810d52d0f
trainee <trainee>
2017-04-12 15:16:13 +0700

    java-8-streams-part3

--- Java-8-streams-part3/src/main/java/com/mitrais/cdc/java/MathUtils.java
+++ Java-8-streams-part3/src/main/java/com/mitrais/cdc/java/MathUtils.java
@@ -14,7 +14,8 @@ public class MathUtils {
     //todo:
     // Make a very large array of random doubles, each of which ranges from 0 to 1. A quick and easy
     //way to do this is with “new Random().doubles(size).toArray()”.
-    return 0;
+	  
+    return new Random().doubles(size).toArray();
   }
   
   /** Returns the sum of the square roots of entries in the stream. 
@@ -24,8 +25,8 @@ public class MathUtils {
     // TODO:
     //Compute the sum of the square roots of the numbers in the array. Find a shorter and simpler way
     //than making a loop to tally the sum
-
-    return 0;
+	  
+    return numStream.map(d -> Math.sqrt(d)).sum();
   }
   
   /** Returns the sum of the square roots of entries in the stream. 
@@ -34,14 +35,31 @@ public class MathUtils {
   public static double sqrtSumParallel(DoubleStream numStream) {
     // TODO:
     //Repeat the process at sqrtSum method in parallel
-
-    return 0;
+	  return numStream.parallel().map(d -> Math.sqrt(d)).sum();
   }
   
   // Or, use DoubleStream.generate, which produces DoubleStream
   // instead of Stream<Double>
   public static Stream<Double> randomNums(double maxValue) {
     //todo
-    return 0;
+	  //infinite stream
+	 return DoubleStream.generate(() -> Math.random()*maxValue).boxed();
   }
+  
+  public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	  System.out.println("Random 10 Double List:");
+		List<Double> randList = randomNums(10).limit(10).collect(Collectors.toList());
+		randList.stream().forEach(r -> System.out.println(r));
+		
+		System.out.println("Random 5 Double Array :");
+		Double[] randArr = randomNums(10).limit(5).toArray(Double[]::new);
+		for(Double r:randArr)
+			System.out.println(r);
+		
+		System.out.println("Random 20 Double Array :");
+		Double[] randArr20 = randomNums(10).limit(20).toArray(Double[]::new);
+		for(Double r20:randArr20)
+			System.out.println(r20);
+	}
 }
