bb82ae73b0923d2ca64ff232364a386d72c27a2b
trainee <trainee>
2017-04-12 17:06:52 +0700

    Java Lambda Collection

--- lambdas-in-collection/src/main/java/com/mitrais/cdc/java/Utils.java
+++ lambdas-in-collection/src/main/java/com/mitrais/cdc/java/Utils.java
@@ -1,80 +1,94 @@
 package com.mitrais.cdc.java;
 
 import java.util.*;
+import java.util.stream.Stream;
 
 public class Utils {
-  
-  public static List<Double> randomNumberList(int size) {
-	  
-	  /*
-	   * TO DO 
-	   * Make a static method that, given a size, will produce a List<Double> of that size, 
-	   * each value of which is a random number between 0 and 1. 
-	   * Use Math.random() and normal List methods.
-	   *
-	   */
-	  return null;
-  }
-  
-  public static double average(List<Double> nums) {
-	
-	  
-	  /*
-	   * TO DO
-	   * Make a static method that, given a List<Double>, will output the average.
-	   * Use normal List methods.
-	   * (You can also use Stream methods if you can figure out 
-	   * how to turn a List<Double> into a DoubleStream.)
-	   * 
-	   */
-	  return 0;
-  }
-  
-  public static void removeBelow(List<Double> nums, double cutoff) {
-	  
-	  /*
-	   * TO DO
-	   * Make a static method that, given a List<Double> and a cutoff, 
-	   * will modify the List so that all numbers
-	   * below the cutoff are removed. 
-	   * Use one of the methods from this lecture.
-	   * 
-	   */
-  }
-  
-  public static void doubleAllValues(List<Double> nums) {
-	  
-	  /*
-	   * TO DO
-	   * Make a static method that, given a List<Double>, 
-	   * will modify the List by doubling (multiplying by 2) all the values.
-	   * Use one of the methods from this lecture
-	   * 
-	   */	  
-	  nums.replaceAll(n -> n*2);
-  }
-  
-  /** Returns the number of primes from 0 to this number, inclusive. */
-  
-  public static int countPrimes1(int upperBound) {
-    if (upperBound <= 2) {
-      return(1);
-    }
-    if (Primes.isPrime(upperBound)) {
-      return(1 + countPrimes1(upperBound - 1));
-    } else {
-      return(countPrimes1(upperBound - 1));
-    }
-  }
-  
-  public static int countPrimes(int upperBound) {
-	  
-	  /*
-	   * TO DO
-	   * Use computeIfAbsent to make a memoized version of countPrimes1 method.
-	   * Profile it and compare performance
-	   *
-	   */
-	  return 0;
-  }
+	private static Map<Integer, Integer> primes = new HashMap<>();
+
+	public static List<Double> randomNumberList(int size) {
+
+		/*
+		 * TO DO Make a static method that, given a size, will produce a
+		 * List<Double> of that size, each value of which is a random number
+		 * between 0 and 1. Use Math.random() and normal List methods.
+		 */
+		List<Double> list = new ArrayList<Double>(size);
+		for (int i = 0; i < size; i++) {
+			list.add(Math.random());
+		}
+		
+		return list;
+	}
+
+	public static double average(List<Double> nums) {
+
+		/*
+		 * TO DO Make a static method that, given a List<Double>, will output
+		 * the average. Use normal List methods. (You can also use Stream
+		 * methods if you can figure out how to turn a List<Double> into a
+		 * DoubleStream.)
+		 * 
+		 */
+		double res = 0;
+		for (Double num : nums) {
+			res += num;
+		}
+		return res / nums.size();
+	}
+
+	public static void removeBelow(List<Double> nums, double cutoff) {
+
+		/*
+		 * TO DO Make a static method that, given a List<Double> and a cutoff,
+		 * will modify the List so that all numbers below the cutoff are
+		 * removed. Use one of the methods from this lecture.
+		 * 
+		 */
+		nums.removeIf(s -> s < cutoff);
+	}
+
+	public static void doubleAllValues(List<Double> nums) {
+
+		/*
+		 * TO DO Make a static method that, given a List<Double>, will modify
+		 * the List by doubling (multiplying by 2) all the values. Use one of
+		 * the methods from this lecture
+		 * 
+		 */
+		nums.replaceAll(n -> n * 2);
+	}
+
+	/** Returns the number of primes from 0 to this number, inclusive. */
+
+	public static int countPrimes1(int upperBound) {
+		if (upperBound <= 2) {
+			return (1);
+		}
+		if (Primes.isPrime(upperBound)) {
+			return (1 + countPrimes1(upperBound - 1));
+		} else {
+			return (countPrimes1(upperBound - 1));
+		}
+	}
+
+	public static int countPrimes(int upperBound) {
+
+		/*
+		 * TO DO Use computeIfAbsent to make a memoized version of countPrimes1
+		 * method. Profile it and compare performance
+		 *
+		 */
+		return primes.computeIfAbsent(upperBound, n -> {
+			if (n <= 2) {
+				return (1);
+			}
+			if (Primes.isPrime(n)) {
+				return (1 + countPrimes(n - 1));
+			} else {
+				return (countPrimes(n - 1));
+			}
+
+		});
+	}
 }
--- lambdas-in-collection/src/main/java/com/mitrais/cdc/java/UtilsExamples.java
+++ lambdas-in-collection/src/main/java/com/mitrais/cdc/java/UtilsExamples.java
@@ -1,52 +1,59 @@
 package com.mitrais.cdc.java;
 
+import java.util.List;
+
 import com.mitrais.cdc.java.timing.Op;
 
-public class UtilsExamples { 
-  
-  public static double showAverages1(int size, double cutoff) {
-	  
-	  /* 
-	   * TO DO
-	   * Make a large list containing random numbers between 0 and 1.
-	   * Compute the average and verify that it is near to 0.5. 
-	   * Remove the numbers less than 0.5. 
-	   * Compute the average again and verify that it is near to 0.75.
-	   *
-	   */
-	  return 0;
-  }
-  
-  public static double showAverages2(int size) {
-	  
-	  /*
-	   * TO DO
-	   * Make a large list containing random numbers between 0 and 1. 
-	   * Compute the average and verify that it is near to 0.5.
-	   * Double all the values.
-	   * Compute the average again and verify that it is near to 1.0.
-	   * 
-	   */
+public class UtilsExamples {
 
-	  return 0;
-    
-  }
-  
-  private static int cutoff = 1_000;
-  
-  public static boolean profilePrimeCounts() {
-	  double primes1Time = 0;
-	  double primesTime = 0;
-	  
-	  for(int i=0; i<9; i++) {
-	      primes1Time = Op.timeOp(() -> System.out.printf("Number of primes up to %,d is %,d [version 1].%n", cutoff, Utils.countPrimes1(cutoff)));
-	      primesTime = Op.timeOp(() -> System.out.printf("Number of primes up to %,d is %,d [version 2].%n", cutoff, Utils.countPrimes(cutoff)));
-	      cutoff = cutoff + 1_000;
-	      primes1Time += primes1Time;
-	      primesTime +=primesTime;
-	  }
-	  
-	  return (primes1Time > primesTime);
-  }  
-  
+	public static double showAverages1(int size, double cutoff) {
+
+		/*
+		 * TO DO Make a large list containing random numbers between 0 and 1.
+		 * Compute the average and verify that it is near to 0.5. Remove the
+		 * numbers less than 0.5. Compute the average again and verify that it
+		 * is near to 0.75.
+		 *
+		 */
+		List<Double> lists = Utils.randomNumberList(size);
+		Utils.average(lists);
+		Utils.removeBelow(lists, cutoff);
+		return Utils.average(lists);
+	}
+
+	public static double showAverages2(int size) {
+
+		/*
+		 * TO DO Make a large list containing random numbers between 0 and 1.
+		 * Compute the average and verify that it is near to 0.5. Double all the
+		 * values. Compute the average again and verify that it is near to 1.0.
+		 * 
+		 */
+
+		List<Double> lists = Utils.randomNumberList(size);
+		Utils.average(lists);
+		Utils.doubleAllValues(lists);
+		return Utils.average(lists);
+
+	}
+
+	private static int cutoff = 1_000;
+
+	public static boolean profilePrimeCounts() {
+		double primes1Time = 0;
+		double primesTime = 0;
+
+		for (int i = 0; i < 9; i++) {
+			primes1Time = Op.timeOp(() -> System.out.printf("Number of primes up to %,d is %,d [version 1].%n", cutoff,
+					Utils.countPrimes1(cutoff)));
+			primesTime = Op.timeOp(() -> System.out.printf("Number of primes up to %,d is %,d [version 2].%n", cutoff,
+					Utils.countPrimes(cutoff)));
+			cutoff = cutoff + 1_000;
+			primes1Time += primes1Time;
+			primesTime += primesTime;
+		}
+
+		return (primes1Time > primesTime);
+	}
+
 }
