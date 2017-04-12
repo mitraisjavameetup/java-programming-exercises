9492841b74a4797259789669fec40321b4d2a59a
Ali Firdaus Ghifari <alifirdausghifari@gmail.com>
2017-04-12 11:29:55 +0700

    implement filter, map, reduce, count in stream

--- Java-8-streams-part2/src/main/java/com/mitrais/cdc/java/StreamExamples.java
+++ Java-8-streams-part2/src/main/java/com/mitrais/cdc/java/StreamExamples.java
@@ -1,81 +1,103 @@
 package com.mitrais.cdc.java;
 
 import java.util.*;
+import java.util.stream.IntStream;
 
-/** TO DO
- *  please complete the methods for below capabilities (you should using lambda expression).
- *  1. Produce a single String that is the result of concatenating the uppercase versions of all of the 
- *  Strings. E.g., the result should be "HIHELLOHOLA...". Use a single reduce operation, without using map.
- *  
- *  2. Produce the same String as above, but this time via a map operation that turns the words 
- *  into uppercase, followed by a reduce operation that concatenates them.
- *  
- *  3. Produce a String that is all the words concatenated together, but with commas in between. 
- *  E.g., the result should be "hi,hello,...". 
- *  Note that there is no comma at the beginning, before “hi” 
- *  and also no comma at the end (after the last word)
- *  
- *  4. Find the total number of characters (i.e., sum of the lengths) of the strings in the List
- *  
- *  5. Find the number of words that contain an “h”
+/**
+ * TO DO please complete the methods for below capabilities (you should using
+ * lambda expression). 1. Produce a single String that is the result of
+ * concatenating the uppercase versions of all of the Strings. E.g., the result
+ * should be "HIHELLOHOLA...". Use a single reduce operation, without using map.
+ * 
+ * 2. Produce the same String as above, but this time via a map operation that
+ * turns the words into uppercase, followed by a reduce operation that
+ * concatenates them.
+ * 
+ * 3. Produce a String that is all the words concatenated together, but with
+ * commas in between. E.g., the result should be "hi,hello,...". Note that there
+ * is no comma at the beginning, before “hi” and also no comma at the end (after
+ * the last word)
+ * 
+ * 4. Find the total number of characters (i.e., sum of the lengths) of the
+ * strings in the List
+ * 
+ * 5. Find the number of words that contain an “h”
  */
 
 public class StreamExamples {
-	
+
 	List<String> words;
-	StreamExamples(){
+
+	StreamExamples() {
 		words = Arrays.asList("hi", "hello", "hola", "bye", "goodbye", "adios");
 	}
-	
-	public List<String> getStringList(){
+
+	public List<String> getStringList() {
 		return words;
 	}
-	
-	public String uppercaseWithoutMap(){
+
+	public String uppercaseWithoutMap() {
 		String uppercaseString = null;
-		/* TO DO
-		 * complete this method to answer number 1
-		 * using lambda expression
+		/*
+		 * TO DO complete this method to answer number 1 using lambda expression
 		 */
-		
+		uppercaseString = words.stream()
+				.reduce("", String::concat)
+				.toUpperCase();
+
 		return uppercaseString;
 	}
-	
-	public String uppercaseWithMap(){
+
+	public String uppercaseWithMap() {
 		String uppercaseString = null;
-		/* TO DO
-		 * complete this method to answer number 2
-		 * using lambda expression
+		/*
+		 * TO DO complete this method to answer number 2 using lambda expression
 		 */
-		
+
+		uppercaseString = words.stream()
+				.map(String::toUpperCase)
+				.reduce("", String::concat);
+
 		return uppercaseString;
 	}
-	
-	public String wordsWithComma(){
+
+	public String wordsWithComma() {
 		String convertWords = null;
-		/* TO DO
-		 * complete this method to answer number 3
-		 * using lambda expression
+		/*
+		 * TO DO complete this method to answer number 3 using lambda expression
 		 */
-		
+
+		convertWords = words.stream()
+				.reduce((w, x) -> w + "," + x)
+				.orElse("");
+
 		return convertWords;
 	}
-	
-	public int numberOfCharacters(){
+
+	public int numberOfCharacters() {
 		int numOfChar = 0;
-		/*TO DO
-		 *complete this method to answer number 4
-		 *using lambda expression
+		/*
+		 * TO DO complete this method to answer number 4 using lambda expression
 		 */
+
+		numOfChar = words.stream()
+				.map(s -> s.length())
+				.reduce((w, x) -> w + x)
+				.orElse(0);
+		
 		return numOfChar;
 	}
-	
-	public int numberOfWordsWithH(){
+
+	public int numberOfWordsWithH() {
 		int numOfH = 0;
-		/*TO DO
-		 *complete this method to answer number 5
-		 *using lambda expression
+		/*
+		 * TO DO complete this method to answer number 5 using lambda expression
 		 */
+
+		numOfH = (int) words.stream()
+				.filter(w -> w.contains("h"))
+				.count();
+
 		return numOfH;
 	}
 }
