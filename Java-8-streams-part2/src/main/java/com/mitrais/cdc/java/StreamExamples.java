15c2cf47aaa8a0277fd9be5a779ff8a75c660ee4
trainee <trainee>
2017-04-12 11:59:32 +0700

    java8-stream-2

--- Java-8-streams-part2/src/main/java/com/mitrais/cdc/java/StreamExamples.java
+++ Java-8-streams-part2/src/main/java/com/mitrais/cdc/java/StreamExamples.java
@@ -1,6 +1,7 @@
 package com.mitrais.cdc.java;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 /** TO DO
  *  please complete the methods for below capabilities (you should using lambda expression).
@@ -37,8 +38,10 @@ public class StreamExamples {
 		 * complete this method to answer number 1
 		 * using lambda expression
 		 */
+		uppercaseString = words.stream().reduce("", String::concat);
 		
-		return uppercaseString;
+		
+		return uppercaseString.toUpperCase();
 	}
 	
 	public String uppercaseWithMap(){
@@ -47,7 +50,7 @@ public class StreamExamples {
 		 * complete this method to answer number 2
 		 * using lambda expression
 		 */
-		
+		uppercaseString = words.stream().map(String::toUpperCase).reduce("", String::concat);
 		return uppercaseString;
 	}
 	
@@ -57,7 +60,7 @@ public class StreamExamples {
 		 * complete this method to answer number 3
 		 * using lambda expression
 		 */
-		
+		convertWords = words.stream().reduce("",(a,b)-> a.isEmpty() ? b : a+","+b);
 		return convertWords;
 	}
 	
@@ -67,6 +70,7 @@ public class StreamExamples {
 		 *complete this method to answer number 4
 		 *using lambda expression
 		 */
+		numOfChar = words.stream().reduce("",String::concat).length();
 		return numOfChar;
 	}
 	
@@ -76,6 +80,7 @@ public class StreamExamples {
 		 *complete this method to answer number 5
 		 *using lambda expression
 		 */
+		numOfH = (int) words.stream().filter(e->e.contains("h")).count();
 		return numOfH;
 	}
 }
