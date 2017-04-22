28b4f9f46aa1d6a73a5fb132c9a8e97d9f647f36
trainee <trainee>
2017-04-22 15:36:54 +0700

    jpa exercise

--- jpa/src/main/java/com/mitrais/entity/Period.java
+++ jpa/src/main/java/com/mitrais/entity/Period.java
@@ -2,13 +2,16 @@ package com.mitrais.entity;
 
 import javax.persistence.Column;
 import javax.persistence.Embeddable;
+
 import java.util.Date;
 
 // TODO please add annotation for entity class
+@Embeddable
 public class Period {
     // TODO implement this entity class
+	@Column(name="start_date")
     private Date startDate;
-
+	@Column(name="end_date")
     private Date endDate;
 
     public Period() {
@@ -16,6 +19,20 @@ public class Period {
     }
 
     public void setStartDate(Date startDate) {
-
+    	this.startDate = startDate;
     }
+
+	public Date getEndDate() {
+		return endDate;
+	}
+
+	public void setEndDate(Date endDate) {
+		this.endDate = endDate;
+	}
+
+	public Date getStartDate() {
+		return startDate;
+	}
+    
+    
 }
