23901613170a5a343b02a49b030399752d9d8430
trainee <trainee>
2017-04-22 15:55:51 +0700

    jpa

--- jpa/src/main/java/com/mitrais/entity/EmployeeEntityListener.java
+++ jpa/src/main/java/com/mitrais/entity/EmployeeEntityListener.java
@@ -1,12 +1,16 @@
 package com.mitrais.entity;
 
+import javax.persistence.EntityListeners;
 import javax.persistence.PrePersist;
 import javax.persistence.PreUpdate;
 import java.util.Date;
 
+@EntityListeners(Employee.class)
 public class EmployeeEntityListener  {
     // TODO implement this entity listener class to auto update employee last modified after create/update employee
-    public void setLastModified(final Employee employee) {
-    	
+    @PrePersist
+    @PreUpdate
+	public void setLastModified(final Employee employee) {
+    	employee.setLastModified(new Date());
     }
 }
--- jpa/src/main/java/com/mitrais/entity/ProjectId.java
+++ jpa/src/main/java/com/mitrais/entity/ProjectId.java
@@ -2,11 +2,23 @@ package com.mitrais.entity;
 
 import javax.persistence.Column;
 import javax.persistence.Embeddable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 import java.io.Serializable;
 
 // TODO please add annotation for entity class
+@Entity
+@Table(name="t_project")
 public class ProjectId implements Serializable {
-    // TODO implement this entity class
+   
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private long id;
+	// TODO implement this entity class
     private String projectName;
     private String role;
 
@@ -14,6 +26,31 @@ public class ProjectId implements Serializable {
 
     }
 
-    public ProjectId(String projectName, String role) {
+    public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public ProjectId(String projectName, String role) {
+		this.projectName = projectName;
+		this.role = role;
     }
+    public String getProjectName() {
+		return projectName;
+	}
+
+	public void setProjectName(String projectName) {
+		this.projectName = projectName;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
 }
